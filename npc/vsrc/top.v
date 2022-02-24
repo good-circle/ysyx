2382410bb397ddef1a9859aa2938133b354c0b2e
good-circle <fenghaoyuan19@mails.ucas.ac.cn>
2022-02-23 17:05:24 -0800

    test auto git

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,14 +1,7 @@
 module top(
-  input clk,
-  input rst,
-  output reg [15:0] led
+  input a,
+  input b,
+  output f
 );
-  reg [31:0] count;
-  always @(posedge clk) begin
-    if (rst) begin led <= 1; count <= 0; end
-    else begin
-      if (count == 0) led <= {led[14:0], led[15]};
-      count <= (count >= 50000 ? 32'b0 : count + 1);
-    end
-  end
+  assign f = a ^ b;
 endmodule
