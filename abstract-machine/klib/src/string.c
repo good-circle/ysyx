1d0f57e5b9b0da95b36d882a9be213b2ca56f75b
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-25 08:01:39 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:01:39 up  1:21,  1 user,  load average: 0.31, 0.15, 0.05

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -6,31 +6,70 @@
 
 size_t strlen(const char *s)
 {
-panic("Not implemented");
+    size_t length = 0;
+    for (; *s != '\0'; s++)
+    {
+        length++;
+    }
+    return length;
 }
 
 char *strcpy(char *dst, const char *src)
 {
-panic("Not implemented");
+    char *start = dst;
+    while (*src != '\0')
+    {
+        *dst++ = *src++;
+    }
+    *dst = '\0';
+    return start;
 }
 
 char *strncpy(char *dst, const char *src, size_t n)
 {
-panic("Not implemented");
+    char *start = dst;
+    while (n > 0)
+    {
+        *dst++ = *src++;
+        n--;
+    }
+    return start;
 }
 
 char *strcat(char *dst, const char *src)
 {
-panic("Not implemented");
+    char *start = dst;
+    while (*dst != '\0')
+    {
+        dst++;
+    }
+    while (*src != '\0')
+    {
+        *dst++ = *src++;
+    }
+    *dst = '\0';
+    return start;
 }
+
 int strcmp(const char *s1, const char *s2)
 {
-panic("Not implemented");
+    while (*s1 != '\0' && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+    }
+    return (int)(*s1 - *s2);
 }
 
 int strncmp(const char *s1, const char *s2, size_t n)
 {
-panic("Not implemented");
+    while (n > 1 && *s1 != '\0' && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+        n--;
+    }
+    return (int)(*s1 - *s2);
 }
 
 void *memset(void *s, int c, size_t n)
