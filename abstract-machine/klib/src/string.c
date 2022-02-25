0d7165ab6795638a5b7581cba377b28183048212
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-25 07:59:11 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:59:11 up  1:19,  1 user,  load average: 0.21, 0.07, 0.02

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,44 +4,93 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  panic("Not implemented");
+size_t strlen(const char *s)
+{
+    size_t length = 0;
+    for (; *s != '\0'; s++)
+    {
+        length++;
+    }
+    return length;
 }
 
-char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+char *strcpy(char *dst, const char *src)
+{
+    char *start = dst;
+    while (*src != '\0')
+    {
+        *dst++ = *src++;
+    }
+    *dst = '\0';
+    return start;
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+char *strncpy(char *dst, const char *src, size_t n)
+{
+    char *start = dst;
+    while (n > 0)
+    {
+        *dst++ = *src++;
+        n--;
+    }
+    return start;
 }
 
-char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+char *strcat(char *dst, const char *src)
+{
+    char *start = dst;
+    while (*dst != '\0')
+    {
+        dst++;
+    }
+    while (*src != '\0')
+    {
+        *dst++ = *src++;
+    }
+    *dst = '\0';
+    return start;
 }
 
-int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+int strcmp(const char *s1, const char *s2)
+{
+    while (*s1 != '\0' && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+    }
+    return (int)(*s1 - *s2);
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+    while (n > 1 && *s1 != '\0' && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+        n--;
+    }
+    return (int)(*s1 - *s2);
 }
 
-void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+void *memset(void *s, int c, size_t n)
+{
+    panic("Not implemented");
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+void *memmove(void *dst, const void *src, size_t n)
+{
+    panic("Not implemented");
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+void *memcpy(void *out, const void *in, size_t n)
+{
+    panic("Not implemented");
 }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+    panic("Not implemented");
 }
 
 #endif
+
