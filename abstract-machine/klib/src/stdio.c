9fc860dd2c9afdb81770c19c91eea0136953d615
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-25 20:23:12 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:23:11 up 4 min,  1 user,  load average: 0.93, 1.21, 0.59

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,24 +5,97 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+int printf(const char *fmt, ...)
+{
+    panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int vsprintf(char *out, const char *fmt, va_list ap)
+{
+    int pos = 0;
+    int length = 0;
+    for (; *fmt != '\0'; fmt++)
+    {
+        while (*fmt != '%' && *fmt != '\0')
+        {
+            out[pos++] = *fmt++;
+        }
+
+        if (*fmt == '%')
+        {
+            fmt++;
+        }
+        else if (*fmt == '\0')
+        {
+            break;
+        }
+
+        switch (*fmt)
+        {
+        case 's':
+        {
+            char *s = va_arg(ap, char *);
+            while (*s != '\0')
+            {
+                out[pos++] = *s++;
+            }
+            break;
+        }
+
+        case 'd':
+        {
+            int d = va_arg(ap, int);
+            if (d < 0)
+            {
+                d = -d;
+                out[pos++] = '-';
+            }
+            char num[20] = {0};
+            int div = d;
+            int rem = 0;
+            while (div > 0)
+            {
+                div = d / 10;
+                rem = d % 10;
+                num[length++] = rem + '0';
+            }
+            if (length == 0)
+            {
+                out[pos++] = '0';
+            }
+            else
+            {
+                for (; length > 0; length--)
+                {
+                    out[pos++] = num[length];
+                }
+            }
+            break;
+        }
+        }
+    }
+    out[pos] = '\0';
+    return pos;
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+int sprintf(char *out, const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    int length = vsprintf(out, fmt, ap);
+    va_end(ap);
+    return length;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
+    panic("Not implemented");
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+{
+    panic("Not implemented");
 }
 
 #endif
+
