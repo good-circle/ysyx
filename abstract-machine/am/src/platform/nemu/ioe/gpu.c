756ce2d66595df0fa2aeaf6537381d1dc43c2bb5
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-27 07:31:19 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:31:19 up 11:11,  1 user,  load average: 0.31, 0.12, 0.04

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,25 +1,58 @@
 #include <am.h>
 #include <nemu.h>
+#include <string.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-void __am_gpu_init() {
-}
+static int Width = 0;
+static int Height = 0;
 
-void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
-}
-
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
+void __am_gpu_init()
+{
+    int i = 0;
+    int width_height = inl(VGACTL_ADDR);
+    int w = width_height >> 16;
+    int h = width_height & 0xffff;
+    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    for (i = 0; i < w * h; i++)
+    {
+        fb[i] = i;
+    }
     outl(SYNC_ADDR, 1);
-  }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
+{
+    *cfg = (AM_GPU_CONFIG_T){
+        .present = true,
+        .has_accel = false,
+        .width = Width,
+        .height = Height,
+        .vmemsz = 0};
+}
+
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
+{
+    int x = ctl->x;
+    int y = ctl->y;
+    uint32_t *pixels = ctl->pixels;
+    int w = ctl->w;
+    int h = ctl->h;
+
+    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    for (int i = 0; i < h; i++)
+    {
+        memcpy(&fb[(y + i) * Width + x], pixels, 4 * w);
+        pixels += w;
+    }
+
+    if (ctl->sync)
+    {
+        outl(SYNC_ADDR, 1);
+    }
+}
+
+void __am_gpu_status(AM_GPU_STATUS_T *status)
+{
+    status->ready = true;
 }
