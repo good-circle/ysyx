1b5673050208bc79b60c4f8ca9e26b647290cdda
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-26 23:36:11 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:36:11 up  3:16,  1 user,  load average: 0.26, 0.24, 0.31

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,23 +1,25 @@
 #include <am.h>
 #include <nemu.h>
 #include <stdio.h>
+
 static uint64_t boot_time = 0;
 
-
-static uint64_t read_time() {
-  uint32_t lo = *(volatile uint32_t *)(RTC_ADDR);
-  uint32_t hi = *(volatile uint32_t *)(RTC_ADDR + 4);
-  uint64_t time = lo + hi * 0;
-  printf("%d\n", lo);
-  return time;
+static uint64_t read_time()
+{
+    uint32_t lo = *(volatile uint32_t *)(RTC_ADDR);
+    uint32_t hi = *(volatile uint32_t *)(RTC_ADDR + 4);
+    uint64_t time = ((uint64_t)hi << 32) | lo;
+    return time;
 }
 
-void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = read_time() - boot_time;
+void __am_timer_init()
+{
+    boot_time = read_time();
 }
 
-void __am_timer_init() {
-  boot_time = read_time();
+void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
+{
+    uptime->us = read_time() - boot_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc)
