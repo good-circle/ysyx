d8b55b09df28fa04977fda6fcaebd8775920726c
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-27 04:15:24 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:15:24 up  7:55,  1 user,  load average: 0.00, 0.03, 0.00

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,41 +5,60 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
-int rand(void) {
-  // RAND_MAX assumed to be 32767
-  next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+int rand(void)
+{
+    // RAND_MAX assumed to be 32767
+    next = next * 1103515245 + 12345;
+    return (unsigned int)(next / 65536) % 32768;
 }
 
-void srand(unsigned int seed) {
-  next = seed;
+void srand(unsigned int seed)
+{
+    next = seed;
 }
 
-int abs(int x) {
-  return (x < 0 ? -x : x);
+int abs(int x)
+{
+    return (x < 0 ? -x : x);
 }
 
-int atoi(const char* nptr) {
-  int x = 0;
-  while (*nptr == ' ') { nptr ++; }
-  while (*nptr >= '0' && *nptr <= '9') {
-    x = x * 10 + *nptr - '0';
-    nptr ++;
-  }
-  return x;
+int atoi(const char *nptr)
+{
+    int x = 0;
+    while (*nptr == ' ')
+    {
+        nptr++;
+    }
+    while (*nptr >= '0' && *nptr <= '9')
+    {
+        x = x * 10 + *nptr - '0';
+        nptr++;
+    }
+    return x;
 }
 
-void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
-  return NULL;
+static char *Free = NULL;
+
+void *malloc(size_t size)
+{
+    if(Free == NULL)
+    {
+        Free = ROUNDUP(heap.start, 8);
+    }
+    
+    size = ROUNDUP(size, 8);
+    Free += size;
+
+    if(Free >= heap.end)
+    {
+        assert(0);
+    }
+
+    return Free - size;
 }
 
-void free(void *ptr) {
+void free(void *ptr)
+{
 }
 
 #endif
