50fed92cff259ecbb2b4b00b8384e8bd9ec5362d
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-26 23:19:52 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:19:52 up  3:00,  1 user,  load average: 0.11, 0.27, 0.33

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -3,12 +3,10 @@
 #include <stdio.h>
 static uint64_t boot_time = 0;
 
-#define CLINT_MMIO 0x2000000ul
-#define TIME_BASE 0xbff8
 
 static uint64_t read_time() {
-  uint32_t lo = *(volatile uint32_t *)(CLINT_MMIO + TIME_BASE + 0);
-  uint32_t hi = *(volatile uint32_t *)(CLINT_MMIO + TIME_BASE + 4);
+  uint32_t lo = *(volatile uint32_t *)(RTC_ADDR);
+  uint32_t hi = *(volatile uint32_t *)(RTC_ADDR + 4);
   uint64_t time = ((uint64_t)hi << 32) | lo;
   return time / 10;
 }
