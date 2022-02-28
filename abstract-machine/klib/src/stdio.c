9f5ad3dd95a97af88aebd7a40fc9f7f0e259793c
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-28 05:25:08 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:25:08 up  7:56,  1 user,  load average: 0.55, 0.54, 0.33

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -9,7 +9,7 @@ int printf(const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
-    char out[128];
+    char out[4096];
     int length = vsprintf(out, fmt, ap);
     for (int i = 0; i < length; i++)
     {
