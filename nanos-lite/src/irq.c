6915b7ea532f3e8d8cba362e6c1945408d07467e
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-01 02:48:38 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:48:38 up 1 day,  5:00,  1 user,  load average: 0.49, 0.34, 0.24

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,14 +1,21 @@
 #include <common.h>
 
-static Context* do_event(Event e, Context* c) {
-  switch (e.event) {
-    default: panic("Unhandled event ID = %d", e.event);
-  }
+static Context *do_event(Event e, Context *c)
+{
+    switch (e.event)
+    {
+    case EVENT_YIELD:
+        printf("a sentence");
+        break;
+    default:
+        panic("Unhandled event ID = %d", e.event);
+    }
 
-  return c;
+    return c;
 }
 
-void init_irq(void) {
-  Log("Initializing interrupt/exception handler...");
-  cte_init(do_event);
+void init_irq(void)
+{
+    Log("Initializing interrupt/exception handler...");
+    cte_init(do_event);
 }
