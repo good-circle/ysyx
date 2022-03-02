0784954898863be0d000dd02ab22e659f77a6c52
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-02 07:41:50 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:41:50 up 10:07,  1 user,  load average: 0.57, 0.25, 0.13

--- abstract-machine/scripts/riscv64-nemu.mk
+++ abstract-machine/scripts/riscv64-nemu.mk
@@ -2,7 +2,7 @@ include $(AM_HOME)/scripts/isa/riscv64.mk
 include $(AM_HOME)/scripts/platform/nemu.mk
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
 
-override ARGS += -b
+ARGS += -b
 
 AM_SRCS += riscv/nemu/start.S \
            riscv/nemu/cte.c \
