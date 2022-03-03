b45c245aa3a221971fe68d2d33a0c2afc0ad4867
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-03 00:11:21 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:11:21 up  4:47,  1 user,  load average: 0.19, 0.17, 0.08

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -3,54 +3,75 @@
 #include <device/mmio.h>
 #include <isa.h>
 
-#if   defined(CONFIG_PMEM_MALLOC)
+#if defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
 #else // CONFIG_PMEM_GARRAY
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
-uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+uint8_t *guest_to_host(paddr_t paddr)
+{
+    return pmem + paddr - CONFIG_MBASE;
+}
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
-static word_t pmem_read(paddr_t addr, int len) {
-  word_t ret = host_read(guest_to_host(addr), len);
-  return ret;
+static word_t pmem_read(paddr_t addr, int len)
+{
+    word_t ret = host_read(guest_to_host(addr), len);
+    return ret;
 }
 
-static void pmem_write(paddr_t addr, int len, word_t data) {
-  host_write(guest_to_host(addr), len, data);
+static void pmem_write(paddr_t addr, int len, word_t data)
+{
+    host_write(guest_to_host(addr), len, data);
 }
 
-static void out_of_bound(paddr_t addr) {
-  panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
-      addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc);
+static void out_of_bound(paddr_t addr)
+{
+    panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
+          addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc);
 }
 
-void init_mem() {
-#if   defined(CONFIG_PMEM_MALLOC)
-  pmem = malloc(CONFIG_MSIZE);
-  assert(pmem);
+void init_mem()
+{
+#if defined(CONFIG_PMEM_MALLOC)
+    pmem = malloc(CONFIG_MSIZE);
+    assert(pmem);
 #endif
 #ifdef CONFIG_MEM_RANDOM
-  uint32_t *p = (uint32_t *)pmem;
-  int i;
-  for (i = 0; i < (int) (CONFIG_MSIZE / sizeof(p[0])); i ++) {
-    p[i] = rand();
-  }
+    uint32_t *p = (uint32_t *)pmem;
+    int i;
+    for (i = 0; i < (int)(CONFIG_MSIZE / sizeof(p[0])); i++)
+    {
+        p[i] = rand();
+    }
 #endif
-  Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]",
-      (paddr_t)CONFIG_MBASE, (paddr_t)CONFIG_MBASE + CONFIG_MSIZE);
+    Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]",
+        (paddr_t)CONFIG_MBASE, (paddr_t)CONFIG_MBASE + CONFIG_MSIZE);
 }
 
-word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  out_of_bound(addr);
-  return 0;
+word_t paddr_read(paddr_t addr, int len)
+{
+#ifdef CONFIG_MTRACE
+    printf("memory read at 0x%x of length 0x%x", addr, len);
+#endif
+    if (likely(in_pmem(addr)))
+        return pmem_read(addr, len);
+    IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+    out_of_bound(addr);
+    return 0;
 }
 
-void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-  out_of_bound(addr);
+void paddr_write(paddr_t addr, int len, word_t data)
+{
+#ifdef CONFIG_MTRACE
+    printf("memory write at 0x%x of length 0x%x", addr, len);
+#endif
+    if (likely(in_pmem(addr)))
+    {
+        pmem_write(addr, len, data);
+        return;
+    }
+    IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return );
+    out_of_bound(addr);
 }
