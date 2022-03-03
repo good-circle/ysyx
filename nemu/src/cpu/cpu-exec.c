b4d9fe18ee732e6e309086b6d2f7434fc79281d7
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-02 21:12:13 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:12:13 up  1:48,  1 user,  load average: 0.54, 0.54, 0.41

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -163,7 +163,7 @@ void cpu_exec(uint64_t n)
         Log("nemu: %s at pc = " FMT_WORD,
             (nemu_state.state == NEMU_ABORT ? ASNI_FMT("ABORT", ASNI_FG_RED) : (nemu_state.halt_ret == 0 ? ASNI_FMT("HIT GOOD TRAP", ASNI_FG_GREEN) : ASNI_FMT("HIT BAD TRAP", ASNI_FG_RED))),
             nemu_state.halt_pc);
-        if (nemu_state.state == NEMU_ABORT || nemu_state.halt_ret != 0 || nemu_state.state==NEMU_END)
+        if (nemu_state.state == NEMU_ABORT || nemu_state.halt_ret != 0)
         {
             print_iring();
         }
