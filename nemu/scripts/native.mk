f4cbea9dde45a67f915be8a86d52c5c5f81604fe
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-03 09:21:00 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:21:00 up 12:17,  1 user,  load average: 0.56, 0.29, 0.25

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -9,8 +9,8 @@ $(BINARY): compile_git
 
 # Some convenient rules
 
-override ARGS ?=
-override ARGS += --log=$(BUILD_DIR)/nemu-log.txt
+#override ARGS ?=
+override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
