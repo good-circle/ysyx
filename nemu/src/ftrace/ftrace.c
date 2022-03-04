d89c7b0a447c661f2f45ab846de150babb1c317b
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-04 08:45:08 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:45:08 up  2:36,  1 user,  load average: 0.34, 0.25, 0.14

--- nemu/src/ftrace/ftrace.c
+++ nemu/src/ftrace/ftrace.c
@@ -28,11 +28,11 @@ void init_ftrace(const char *elf_file)
     fclose(fp);
 
     /* read section headers */
-    Elf64_Shdr *shdr = (Elf64_Shdr *)(ehdr + ehdr->e_shoff);
+    //Elf64_Shdr *shdr = (Elf64_Shdr *)(ehdr + ehdr->e_shoff);
 
     //printf("%d\n", ehdr->e_shnum);
     //for(int i = 0 ; i < ehdr->e_shnum; i++)
     //{
-        printf("%d\n", shdr[0].sh_name);
+        //printf("%d\n", shdr[0].sh_name);
     //}
 }
