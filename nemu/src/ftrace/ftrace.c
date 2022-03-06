1b9a2446bda6d98731fb17000a63db9d19731a2c
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-05 21:44:48 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:44:48 up 36 min,  1 user,  load average: 0.36, 0.10, 0.07

--- nemu/src/ftrace/ftrace.c
+++ nemu/src/ftrace/ftrace.c
@@ -1,6 +1,15 @@
 #include <common.h>
 #include <elf.h>
 
+typedef struct {
+  Elf64_Word	st_name;
+  unsigned char	st_info;
+  unsigned char st_other;
+  Elf64_Section	st_shndx;
+  Elf64_Addr	st_value;
+  Elf64_Xword	st_size;
+} Elf64_Func;
+
 void init_ftrace(const char *elf_file)
 {
     /* return if there is no elf_file */
@@ -36,13 +45,12 @@ void init_ftrace(const char *elf_file)
         if (shdr[i].sh_type == SHT_SYMTAB)
         {
             //Elf64_Sym *sym = (Elf64_Sym *)((word_t)ehdr + shdr[i].sh_offset);
-            int sym_num = shdr[i].sh_size / sizeof(Elf64_Sym);
-            printf("%d\n", sym_num);
+            //int sym_num = shdr[i].sh_size / sizeof(Elf64_Sym);
         }
         else if (shdr[i].sh_type == SHT_STRTAB && i != ehdr->e_shstrndx)
         {
-            char *strtab =(char *)((word_t)ehdr + shdr[i].sh_offset);
-            printf("%s\n", strtab);
+            //char *strtab =(char *)((word_t)ehdr + shdr[i].sh_offset);
         }
+        printf("%d\n", shdr[i].sh_name);
     }
 }
