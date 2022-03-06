7fd68be5a0e5da027ed8951ffbc8c6c3e60eee57
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-06 01:11:51 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:11:51 up  3:50,  1 user,  load average: 0.58, 0.46, 0.26

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -93,16 +93,16 @@ static void exec_once(Decode *s, vaddr_t pc)
         memset(ftrace_pos, ' ', call_depth);
         ftrace_pos += call_depth;
         call_depth++;
+        printf("%d\n", func_num);
         for (int i = 0; i < func_num; i++)
         {
-            assert(0);
-            //printf("%d\n", func_num);
-            //if (s->dnpc >= func[i].st_value && s->dnpc < func[i].st_value + func[i].st_size)
-            //{
-            //    printf("call [%s@0x%16lx]\n", (char *)((word_t)strtab + func[i].st_name), s->dnpc);
-            //    ftrace_pos += sprintf(ftrace_pos, "call [%s@0x%16lx]\n", (char *)((word_t)strtab + func[i].st_name), s->dnpc);
-            //    break;
-            //}
+            printf("%d\n", func_num);
+            if (s->dnpc >= func[i].st_value && s->dnpc < func[i].st_value + func[i].st_size)
+            {
+                printf("call [%s@0x%16lx]\n", (char *)((word_t)strtab + func[i].st_name), s->dnpc);
+                ftrace_pos += sprintf(ftrace_pos, "call [%s@0x%16lx]\n", (char *)((word_t)strtab + func[i].st_name), s->dnpc);
+                break;
+            }
         }
     }
     else if (s->ftrace == JALR && BITS(s->isa.inst.val, 19, 15) == 1)
@@ -113,7 +113,6 @@ static void exec_once(Decode *s, vaddr_t pc)
         call_depth--;
         for (int i = 0; i < func_num; i++)
         {
-            assert(0);
             if (s->dnpc >= func[i].st_value && s->dnpc < func[i].st_value + func[i].st_size)
             {
                 ftrace_pos += sprintf(ftrace_pos, "ret  [%s@0x%16lx]\n", (char *)((word_t)strtab + func[i].st_name), s->dnpc);
