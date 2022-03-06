a7e5b337a00b2dce054580c31cd5e5917b424e7f
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-06 01:07:17 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:07:17 up  3:45,  1 user,  load average: 0.09, 0.16, 0.11

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -89,6 +89,7 @@ static void exec_once(Decode *s, vaddr_t pc)
     has_ftrace = false;
     if ((s->ftrace == JAL || s->ftrace == JALR) && BITS(s->isa.inst.val, 11, 7) == 1)
     {
+        assert(0);
         has_ftrace = true;
         memset(ftrace_pos, ' ', call_depth);
         ftrace_pos += call_depth;
@@ -97,7 +98,6 @@ static void exec_once(Decode *s, vaddr_t pc)
         {
             if (s->dnpc >= func[i].st_value && s->dnpc < func[i].st_value + func[i].st_size)
             {
-                assert(0);
                 printf("call [%s@0x%16lx]", (char *)((word_t)strtab + func[i].st_name), s->dnpc);
                 ftrace_pos += sprintf(ftrace_pos, "call [%s@0x%16lx]", (char *)((word_t)strtab + func[i].st_name), s->dnpc);
                 break;
