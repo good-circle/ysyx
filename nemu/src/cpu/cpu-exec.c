598c11cfd3ac3cd12bef8e52ad6374c5b9521bda
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-06 01:19:12 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:19:12 up  3:57,  1 user,  load average: 0.45, 0.38, 0.29

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -93,6 +93,7 @@ static void exec_once(Decode *s, vaddr_t pc)
         memset(ftrace_pos, ' ', call_depth);
         ftrace_pos += call_depth;
         call_depth++;
+        printf("%d\n", func_num);
         for (int i = 0; i < func_num; i++)
         {
             if (s->dnpc >= func[i].st_value && s->dnpc < func[i].st_value + func[i].st_size)
