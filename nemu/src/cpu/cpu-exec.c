b115bd9e013480bc1dd0bb99596198bb936ff012
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-06 01:05:07 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:05:07 up  3:43,  1 user,  load average: 0.25, 0.18, 0.11

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -66,10 +66,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
     iring_num = (iring_num > 15) ? 0 : iring_num;
 #endif
 #ifdef CONFIG_FTRACE
-    if(has_ftrace)
-    {
-        printf("%s\n", ftracebuf);
-    }
 #endif
 #ifdef CONFIG_WATCHPOINT
     if (check_watchpoint())
