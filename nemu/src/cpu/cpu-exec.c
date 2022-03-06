a8a538d6d6d9f5e65bfd9db396640bdf84620178
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-06 01:43:45 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:43:45 up  4:22,  1 user,  load average: 0.40, 0.31, 0.28

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -98,7 +98,7 @@ static void exec_once(Decode *s, vaddr_t pc)
         {
             if (s->dnpc >= func[i].st_value && s->dnpc < func[i].st_value + func[i].st_size)
             {
-                ftrace_pos += sprintf(ftrace_pos, "call [%s@0x%16lx]\n", (char *)((word_t)strtab + func[i].st_name), s->dnpc);
+                ftrace_pos += sprintf(ftrace_pos, "call [%s@0x%8lx]\n", (char *)((word_t)strtab + func[i].st_name), s->dnpc);
                 break;
             }
         }
@@ -114,7 +114,7 @@ static void exec_once(Decode *s, vaddr_t pc)
         {
             if (s->dnpc >= func[i].st_value && s->dnpc < func[i].st_value + func[i].st_size)
             {
-                ftrace_pos += sprintf(ftrace_pos, "ret  [%s@0x%16lx]\n", (char *)((word_t)strtab + func[i].st_name), s->dnpc);
+                ftrace_pos += sprintf(ftrace_pos, "ret  [%s@0x%8lx]\n", (char *)((word_t)strtab + func[i].st_name), s->dnpc);
                 break;
             }
         }
