1b08580e0b0c5f1795c2eca14c0fca240b109bdd
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 06:23:27 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:23:27 up  5:31,  1 user,  load average: 0.26, 0.17, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <nemu.h>
 #include <stdio.h>
+#include <string.h>
 
 #define AUDIO_FREQ_ADDR (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR (AUDIO_ADDR + 0x04)
@@ -9,7 +10,7 @@
 #define AUDIO_INIT_ADDR (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR (AUDIO_ADDR + 0x14)
 
-//static int pos = 0;
+static int pos = 0;
 
 void __am_audio_init()
 {
@@ -18,7 +19,7 @@ void __am_audio_init()
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg)
 {
   cfg->present = true;
-  //cfg->bufsize = sbuf_size;
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl)
@@ -27,6 +28,7 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl)
   outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
   outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
   outl(AUDIO_INIT_ADDR, true);
+  pos = 0;
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat)
@@ -37,13 +39,31 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat)
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl)
 {
   int sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
+  int count = inl(AUDIO_COUNT_ADDR);
   int len = ctl->buf.end - ctl->buf.start;
-  while(sbuf_size - inl(AUDIO_COUNT_ADDR)< len)
+  int free = sbuf_size - count;
+
+  while (free < len)
   {
     ;
   }
 
-  //memcpy(AUDIO_SBUF_ADDR + pos, ctl->buf.start, len);
+  int *sbuf = (int *)AUDIO_SBUF_ADDR;
+  if (pos + len <= sbuf_size)
+  {
+    memcpy(sbuf + pos, ctl->buf.start, len);
+    pos += len;
+  }
+  else
+  {
+    int first = sbuf_size - pos;
+    memcpy(sbuf + pos, ctl->buf.start, first);
 
+    int second = len - first;
+    memcpy(sbuf, ctl->buf.start + first, second);
+    pos = second;
+  }
 
+  count += len;
+  outl(AUDIO_COUNT_ADDR, count);
 }
