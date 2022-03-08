6828e614c9cfce7e7b152acfc89817fb79cf3903
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 01:16:26 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:16:26 up  3:33,  1 user,  load average: 0.54, 0.22, 0.11

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -4,27 +4,40 @@
 
 enum
 {
-  reg_freq,
-  reg_channels,
-  reg_samples,
-  reg_sbuf_size,
-  reg_init,
-  reg_count,
+  reg_freq,      // freq (from AM)
+  reg_channels,  // channels (from AM)
+  reg_samples,   // samples (from AM)
+  reg_sbuf_size, // sbuf_size (from NEMU, CONFIG_SB_SIZE)
+  reg_init,      // whether is initialized (from AM)
+  reg_count,     // used size of sbuf (controled in NEMU)
   nr_reg
 };
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
+void audio_callback(void *userdata, uint8_t *stream, int len)
+{
+  ;
+}
+
 static void audio_io_handler(uint32_t offset, int len, bool is_write)
 {
-  SDL_AudioSpec s = {};
-  s.freq = 
-  s.format = AUDIO_S16SYS; 
-  s.userdata = NULL;      
-  SDL_InitSubSystem(SDL_INIT_AUDIO);
-  SDL_OpenAudio(&s, NULL);
-  SDL_PauseAudio(0);
+  if (audio_base[reg_init] && is_write)
+  {
+    SDL_AudioSpec s = {};
+    s.freq = audio_base[reg_freq];
+    s.format = AUDIO_S16SYS;
+    s.channels = audio_base[reg_channels];
+    s.samples = audio_base[reg_samples];
+    s.callback = audio_callback;
+    s.userdata = NULL;
+    audio_base[reg_count] = 0;
+
+    SDL_InitSubSystem(SDL_INIT_AUDIO);
+    SDL_OpenAudio(&s, NULL);
+    SDL_PauseAudio(0);
+  }
 }
 
 void init_audio()
