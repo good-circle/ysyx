0dcd86434a1ba96cd3a3d8467930bb65a3d42026
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 07:48:30 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:48:30 up  6:56,  1 user,  load average: 0.41, 0.48, 0.42

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -54,7 +54,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  //check_bound(map, addr);
+  check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
