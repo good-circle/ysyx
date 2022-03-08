623f177d2070ed6991296f803da0fc742df3969b
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 01:25:03 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:25:03 up  3:42,  1 user,  load average: 0.32, 0.34, 0.23

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -27,6 +27,7 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl)
   outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
   outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
   outl(AUDIO_INIT_ADDR, true);
+  printf("%d %d %d\n", ctrl->freq, ctrl->channels, ctrl->samples);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat)
