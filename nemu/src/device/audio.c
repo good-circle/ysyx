6c9da0a0f01fd3867e20fcc0ebe68486392fc001
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 07:50:42 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:50:42 up  6:58,  1 user,  load average: 0.52, 0.50, 0.44

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -83,5 +83,5 @@ void init_audio()
 #endif
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
-  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE * 10, NULL);
 }
