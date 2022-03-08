ea32a9673aef85ee64ffbc8a0cb206db8f3e69c4
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 01:26:36 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:26:36 up  3:43,  1 user,  load average: 0.38, 0.36, 0.24

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -19,6 +19,7 @@ void __am_audio_config(AM_AUDIO_CONFIG_T *cfg)
 {
   cfg->present = true;
   cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
+  printf("%d\n", cfg->bufsize);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl)
