c87d678c7ca2b42c354f8d54df78d55d2069b881
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-10 01:35:43 -0800

    >  sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:35:43 up  5:40,  1 user,  load average: 0.22, 0.23, 0.17

---
+++ npc/verilator/csrc/main.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Vtop.h"
+#include "verilated.h"
+#include <verilated_vcd_c.h>
+
+static u_int8_t pmem[0x8000];
+
+void init_pmem()
+{
+    for (int i = 0; i < 200; i++)
+    {
+        pmem[4 * i] = 0x11;
+        pmem[4 * i + 1] = 0x22;
+        pmem[4 * i + 2] = 0x33;
+        pmem[4 * i + 3] = 0x44;
+    }
+}
+
+int pmem_read(unsigned int pc)
+{
+    return *(u_int32_t *) (pmem + pc - 0x80000000);
+}
+
+int main(int argc, char **argv, char **env)
+{
+    VerilatedContext *contextp = new VerilatedContext;
+    Vtop *top = new Vtop{contextp};
+    contextp->commandArgs(argc, argv);
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *m_trace = new VerilatedVcdC;
+    top->trace(m_trace, 99);
+    m_trace->open("waveform.vcd");
+    init_pmem();
+    top->clk = 1;
+    top->pc = 0x80000000;
+    top->a = 0;
+    int i = 0;
+    for (i = 0; i < 200; i++)
+    {
+        //printf("%x ", top->pc);
+        top->inst = pmem_read(top->pc);
+        //printf("%x\n", top->inst);
+        m_trace->dump(2 * i);
+        top->clk = !top->clk;
+        top->eval();
+        m_trace->dump(2 * i + 1);
+        top->clk = !top->clk;
+        top->eval();
+    }
+    m_trace->close();
+    delete top;
+    delete contextp;
+    return 0;
+}
---
+++ npc/verilator/vsrc/top.v
@@ -0,0 +1,21 @@
+module top(
+  input [31:0] inst,
+  output reg [31:0] pc,
+  input clk,
+  output reg [31:0] a
+);
+
+always @(posedge clk) begin
+    pc <= pc + 4;
+end
+
+wire addi;  
+assign addi = (inst == 32'h44332211);
+
+always @(posedge clk) begin
+    if (addi) begin
+        a <= a + 1;
+    end
+end
+  
+endmodule
