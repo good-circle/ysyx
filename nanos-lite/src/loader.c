c5ca52a5e4818608d2dbaa009eb6ad5ee7d131e3
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-11 04:50:39 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:50:39 up  4:33,  1 user,  load average: 0.29, 0.27, 0.19

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -34,7 +34,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
       memset((void *)(phdr->p_vaddr + phdr->p_filesz), 0, phdr->p_memsz - phdr->p_filesz);
     }
   }
-  printf("%d\n", ehdr->e_entry);
+  printf("%ld\n", ehdr->e_entry);
   return ehdr->e_entry;
 }
 
