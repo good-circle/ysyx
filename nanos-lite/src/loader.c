f66cc5c114bfe9dbe91eab1ac565541138566d25
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-11 05:02:22 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:02:22 up  4:44,  1 user,  load average: 0.40, 0.30, 0.21

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -20,7 +20,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
 
   /* assert if not elf file */
-  printf("%d\n", (size_t)ehdr.e_ident);
+  printf("%x\n", (size_t)ehdr.e_ident);
   assert((size_t)ehdr.e_ident == 0x464c457f);
 
   /* read program header */
