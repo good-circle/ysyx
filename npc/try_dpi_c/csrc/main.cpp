ea31161c68e8ff93212e4b728b11843ad65077ca
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-11 00:45:07 -0800

    >  sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:45:07 up 27 min,  1 user,  load average: 0.23, 0.29, 0.18

--- npc/try_dpi_c/csrc/main.cpp
+++ npc/try_dpi_c/csrc/main.cpp
@@ -21,7 +21,7 @@ void init_pmem()
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
 
-  printf("The image is %s, size = %ld", img_file, size);
+  printf("The image is %s, size = %ld\n", img_file, size);
 
   fseek(fp, 0, SEEK_SET);
   int ret = fread(pmem, size, 1, fp);
