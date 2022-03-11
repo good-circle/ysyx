07996fc16644de7f08f7d60e492d359ca3a5f9f6
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-11 04:44:45 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:44:45 up  4:27,  1 user,  load average: 0.45, 0.35, 0.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,21 +2,44 @@
 #include <elf.h>
 
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
+  Elf_Ehdr *ehdr = malloc(sizeof(Elf_Ehdr));
+  Elf_Phdr *phdr = malloc(sizeof(Elf_Phdr));
+
+  /* read elf from ramdisk */
+  ramdisk_read(ehdr, 0, sizeof(Elf_Ehdr));
+
+  /* assert if not elf file */
+  assert((size_t)ehdr->e_ident == 0x464c457f);
+
+  /* read program header */
+  for (int i = 0; i < ehdr->e_phnum; i++)
+  {
+    ramdisk_read(phdr, ehdr->e_phoff + i * ehdr->e_phentsize, ehdr->e_phentsize);
+    if(phdr->p_type == PT_LOAD)
+    {
+      ramdisk_read((void *)phdr->p_vaddr, phdr->p_offset, phdr->p_filesz);
+      /* padding filesz ~ memsz zero */
+      memset((void *)(phdr->p_vaddr + phdr->p_filesz), 0, phdr->p_memsz - phdr->p_filesz);
+    }
+  }
+
+  return ehdr->e_entry;
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
+void naive_uload(PCB *pcb, const char *filename)
+{
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+  ((void (*)())entry)();
 }
-
