556e1b4d0e79f0b6a789f2689026061ec85eb4f8
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-11 07:49:58 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:49:58 up  7:32,  1 user,  load average: 0.22, 0.16, 0.25

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,9 +4,7 @@ static Context *do_event(Event e, Context *c)
 {
     switch (e.event)
     {
-    case EVENT_YIELD:
-        printf("a sentence\n");
-        break;
+
     default:
         panic("Unhandled event ID = %d", e.event);
     }
