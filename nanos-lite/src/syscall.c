5fb07a6d9d423a2a5f9b37dc103c8846cbcac9e7
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-12 00:45:45 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:45:45 up  1:02,  1 user,  load average: 0.42, 0.15, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,8 @@
 #include <common.h>
 #include "syscall.h"
 
+#define STRACE 1
+
 void do_syscall(Context *c)
 {
     uintptr_t a[4];
@@ -9,7 +11,7 @@ void do_syscall(Context *c)
     a[2] = c->GPR3; // a1
     a[3] = c->GPR4; // a2
 
-#ifdef CONFIG_STRACE
+#ifdef STRACE
   printf("Syscall: a7=%d, a0=%d, a1=%d, a2=%d\n", a[0], a[1], a[2], a[3]);
 #endif
 
