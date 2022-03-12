4fbfdd2b01336ccac6c4e6c9b85b06ba712677ae
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-12 06:04:59 -0800

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:04:59 up  6:21,  1 user,  load average: 0.07, 0.11, 0.05

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -68,7 +68,7 @@ int _open(const char *path, int flags, mode_t mode)
 
 int _write(int fd, void *buf, size_t count)
 {
-    return _syscall_(SYS_write, fd, buf, count);
+    return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
 void *_sbrk(intptr_t increment)
