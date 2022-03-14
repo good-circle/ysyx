9ba86dbe3f1953f513a846e710437bfaf690eb2e
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-13 18:31:33 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:31:33 up  9:53,  1 user,  load average: 0.71, 0.44, 0.39

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -62,7 +62,7 @@ void _exit(int status)
 
 int _open(const char *path, int flags, mode_t mode)
 {
-    return _syscall_(SYS_open, path, flags, mode);
+    return _syscall_(SYS_open, (intptr_t)path, flags, mode);
 }
 
 int _write(int fd, void *buf, size_t count)
