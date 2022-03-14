f755825fe3c0ab8a202fd1c4479bb7e2ae4a82e5
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-13 18:12:57 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:12:57 up  9:34,  1 user,  load average: 0.91, 0.49, 0.36

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -57,6 +57,7 @@ int fs_open(const char *pathname, int flags, int mode)
     {
         if (strcmp(pathname, file_table[i].name) == 0)
         {
+            printf("!!%d!!\n", i);
             return i;
         }
     }
