a331b9369b01ded62387c7b07df30f301496d9d7
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-13 18:15:22 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:15:22 up  9:36,  1 user,  load average: 0.94, 0.65, 0.44

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -5,9 +5,13 @@ int main() {
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
 
+  printf("1\n");
+
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
+
+  printf("2\n");
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
@@ -16,21 +20,29 @@ int main() {
     assert(n == i + 1);
   }
 
+  printf("3\n");
+
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
+
+  printf("4\n");
 
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
 
+  printf("5\n");
+
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
+
+  printf("6\n");
 
   fclose(fp);
 
