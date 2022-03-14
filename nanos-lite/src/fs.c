7691074e4d3d8ef1017acbf53d301867c2bad0fe
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-14 06:10:52 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:10:52 up 45 min,  1 user,  load average: 0.10, 0.09, 0.05

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,12 +1,12 @@
 #include <fs.h>
 
-#define STRACE 1
-
 typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
+size_t serial_write(const void *buf, size_t offset, size_t len);
 
 typedef struct
 {
@@ -43,8 +43,8 @@ size_t invalid_write(const void *buf, size_t offset, size_t len)
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
     [FD_STDIN] = {"stdin", 0, 0, 0, invalid_read, invalid_write},
-    [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, invalid_write},
-    [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},
+    [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
+    [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
@@ -75,7 +75,7 @@ size_t fs_read(int fd, void *buf, size_t len)
     len = file_table[fd].open_offset + len > file_table[fd].size ? file_table[fd].size - file_table[fd].open_offset : len;
 
 #ifdef STRACE
-    printf("FS: read file %s at %d for length %d\n", file_table[fd].name, file_table[fd].open_offset, len);
+    printf("FS: read file %s at %d for length %d\n", file_table[i].name, file_table[fd].open_offset, len);
 #endif
 
     ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
@@ -86,11 +86,17 @@ size_t fs_read(int fd, void *buf, size_t len)
 
 size_t fs_write(int fd, const void *buf, size_t len)
 {
+    if(fd == 1 || fd == 2)
+    {
+        file_table[fd].write(buf, 0, len);
+        return len;
+    }
+    
     /* pay attention: man 2 write: writes "up to" count bytes */
     len = file_table[fd].open_offset + len > file_table[fd].size ? file_table[fd].size - file_table[fd].open_offset : len;
 
 #ifdef STRACE
-    printf("FS: write file %s at %d for length %d\n", file_table[fd].name, file_table[fd].open_offset, len);
+    printf("FS: write file %s at %d for length %d\n", file_table[i].name, file_table[fd].open_offset, len);
 #endif
 
     ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
@@ -121,7 +127,7 @@ size_t fs_lseek(int fd, size_t offset, int whence)
     }
 
 #ifdef STRACE
-    printf("FS: lseek file %s to %d\n", file_table[fd].name, file_table[fd].open_offset);
+    printf("FS: lseek file %s to %d\n", file_table[i].name, file_table[fd].open_offset);
 #endif
 
     /* pay attention: ftell will call lseek */
