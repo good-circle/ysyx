cf530b82c5ab7b9a384bf4178aa5b1fde2626374
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-13 20:21:54 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:21:54 up 11:27,  1 user,  load average: 0.81, 0.65, 0.53

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -41,18 +41,9 @@ void do_syscall(Context *c)
         break;
 
     case SYS_write: // 4
-        if (a[1] == 1 || a[1] == 2)
-        {
-            for (int i = 0; i < a[3]; i++)
-            {
-                putch(*((char *)a[2] + i));
-            }
-            c->GPRx = a[3];
-        }
-        else
-        {
-            c->GPRx = fs_write(a[1], (void *)a[2], a[3]);;
-        }
+
+            c->GPRx = fs_write(a[1], (void *)a[2], a[3]);
+        
         break;
 
     case SYS_close: // 7
