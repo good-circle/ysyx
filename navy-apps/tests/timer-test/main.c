c910495ba3d2e94568e21684b0128a410b159fd6
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-14 08:00:06 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:00:06 up  2:34,  1 user,  load average: 0.48, 0.68, 0.43

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -14,13 +14,11 @@ int main()
 
     int i = 1;
 
-    printf("Test start!\n");
-
     while (1)
     {
         gettimeofday(&new_tv, &new_tz);
-        volatile time_t pass = (new_tv.tv_sec - tv.tv_sec) * 1000000 + new_tv.tv_usec - tv.tv_usec;
-        printf("%d\n", i);
+        time_t pass = (new_tv.tv_sec - tv.tv_sec) * 1000000 + new_tv.tv_usec - tv.tv_usec;
+        //printf("%d\n", i);
         if (pass >= i * 500000)
         {
             i++;
