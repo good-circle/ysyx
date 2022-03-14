0f6a7065817588037e94351ae896e435bef0c634
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-13 20:26:37 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:26:37 up 11:32,  1 user,  load average: 0.53, 0.47, 0.48

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,5 +1,7 @@
 #include <fs.h>
 
+#define STRACE 1
+
 typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 
@@ -73,7 +75,7 @@ size_t fs_read(int fd, void *buf, size_t len)
     len = file_table[fd].open_offset + len > file_table[fd].size ? file_table[fd].size - file_table[fd].open_offset : len;
 
 #ifdef STRACE
-    printf("FS: read file %s at %d for length %d\n", file_table[i].name, file_table[fd].open_offset, len);
+    printf("FS: read file %s at %d for length %d\n", file_table[fd].name, file_table[fd].open_offset, len);
 #endif
 
     ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
@@ -88,7 +90,7 @@ size_t fs_write(int fd, const void *buf, size_t len)
     len = file_table[fd].open_offset + len > file_table[fd].size ? file_table[fd].size - file_table[fd].open_offset : len;
 
 #ifdef STRACE
-    printf("FS: write file %s at %d for length %d\n", file_table[i].name, file_table[fd].open_offset, len);
+    printf("FS: write file %s at %d for length %d\n", file_table[fd].name, file_table[fd].open_offset, len);
 #endif
 
     ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
@@ -119,7 +121,7 @@ size_t fs_lseek(int fd, size_t offset, int whence)
     }
 
 #ifdef STRACE
-    printf("FS: lseek file %s to %d\n", file_table[i].name, file_table[fd].open_offset);
+    printf("FS: lseek file %s to %d\n", file_table[fd].name, file_table[fd].open_offset);
 #endif
 
     /* pay attention: ftell will call lseek */
