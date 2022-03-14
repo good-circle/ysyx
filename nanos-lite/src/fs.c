435f2ef25f404ecc92e99bf03c72f93e4814acd7
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-13 19:43:24 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:43:24 up 10:56,  1 user,  load average: 0.60, 0.67, 0.59

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -77,7 +77,7 @@ size_t fs_read(int fd, void *buf, size_t len)
 size_t fs_write(int fd, const void *buf, size_t len)
 {
     printf("%s: %d %d\n", file_table[fd].name, file_table[fd].open_offset, len);
-    assert(file_table[fd].open_offset + len <= file_table[fd].size);
+    assert(fd <= 2 || file_table[fd].open_offset + len <= file_table[fd].size);
 
     ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
     file_table[fd].open_offset += len;
