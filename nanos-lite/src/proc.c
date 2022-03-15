585f90e86c1f6bbe99a9247484ad24fae88dfa08
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-15 07:26:13 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:26:13 up  6:34,  1 user,  load average: 0.31, 0.14, 0.10

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,7 +30,7 @@ void init_proc()
     Log("Initializing processes...");
 
     // load program here
-    naive_uload(NULL, "nslider");
+    naive_uload(NULL, "/bin/nslider");
 }
 
 Context *schedule(Context *prev)
