5b06c069bb5088a344f1459c44ff433e76050fba
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-15 07:31:05 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:31:05 up  6:39,  1 user,  load average: 0.39, 0.29, 0.17

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -5,6 +5,7 @@
 #include <assert.h>
 #include <time.h>
 #include "syscall.h"
+#include <stdio.h>
 
 // helper macros
 #define _concat(x, y) x##y
@@ -62,6 +63,7 @@ void _exit(int status)
 
 int _open(const char *path, int flags, mode_t mode)
 {
+    printf("path is %s\n", path);
     return _syscall_(SYS_open, (intptr_t)path, flags, mode);
 }
 
