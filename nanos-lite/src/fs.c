50815ea04225bd0be84d71d12e9a0bf5b2bd2478
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-14 23:56:30 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:56:30 up  1:10,  1 user,  load average: 0.40, 0.17, 0.09

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -8,6 +8,7 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 size_t serial_write(const void *buf, size_t offset, size_t len);
 size_t events_read(void *buf, size_t offset, size_t len);
+size_t dispinfo_read(void *buf, size_t offset, size_t len);
 
 //#define STRACE
 
@@ -29,6 +30,7 @@ enum
     FD_STDOUT,
     FD_STDERR,
     FD_EVENT,
+    FD_DISPINFO,
     FD_FB
 };
 
@@ -50,6 +52,7 @@ static Finfo file_table[] __attribute__((used)) = {
     [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
     [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
     [FD_EVENT] = {"/dev/events", 0, 0, 0, events_read, invalid_write},
+    [FD_DISPINFO] = {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
