79bd9c2cd0b8094a8e3bbf8a997dfad4e98b7846
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-17 07:47:51 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:47:51 up  5:04,  1 user,  load average: 1.76, 2.68, 1.77

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -151,6 +151,7 @@ static void execute(uint64_t n)
     {
         exec_once(&s, cpu.pc);
         //printf("%lx\n", cpu.pc);
+        printf(" ");
         g_nr_guest_inst++;
         trace_and_difftest(&s, cpu.pc);
         if (nemu_state.state != NEMU_RUNNING)
