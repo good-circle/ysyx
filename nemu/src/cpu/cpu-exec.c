b3eca3303637b42df36f606aeb8d4aa29ab58b52
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-17 08:11:40 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:11:40 up  5:27,  1 user,  load average: 1.61, 2.39, 2.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -195,7 +195,7 @@ void cpu_exec(uint64_t n)
 
     uint64_t timer_start = get_time();
 
-    execute(100000000);
+    execute(1000000000);
 
     uint64_t timer_end = get_time();
     g_timer += timer_end - timer_start;
