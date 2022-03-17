cace0694badce5551407576e2c15dec54473a41a
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-17 07:35:56 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:35:56 up  4:52,  1 user,  load average: 0.91, 0.81, 0.53

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -56,7 +56,10 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
         log_write("%s\n", _this->logbuf);
     }
 #endif
-
+    if (g_print_step)
+    {
+        IFDEF(CONFIG_ITRACE, puts(_this->logbuf));
+    }
     IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 #ifdef CONFIG_IRINGBUF
     //printf("%s\n", _this->logbuf);
