952538df40451004a12aefb21130e5c883d7ade2
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-17 03:20:28 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:20:28 up 36 min,  1 user,  load average: 0.66, 0.20, 0.07

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,29 +1,65 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <stdio.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
 static const char *keyname[] = {
-  "NONE",
-  _KEYS(keyname)
-};
+    "NONE",
+    _KEYS(keyname)};
 
-int SDL_PushEvent(SDL_Event *ev) {
-  return 0;
+int SDL_PushEvent(SDL_Event *ev)
+{
+    return 0;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+int SDL_PollEvent(SDL_Event *ev)
+{
+    return 0;
 }
 
-int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+int SDL_WaitEvent(SDL_Event *event)
+{
+    int len = 64;
+    char buf[len];
+    while (NDL_PollEvent(buf, len) == 0)
+    {
+        ;
+    }
+
+    char ndl_keydown[4];
+    char ndl_keyname[16];
+
+    sscanf(buf, "%s %s\n", ndl_keydown, ndl_keyname);
+
+    if (strcmp(ndl_keydown, "kd") == 0)
+    {
+        event->type = SDL_KEYDOWN;
+    }
+    else
+    {
+        event->type = SDL_KEYUP;
+    }
+
+    for (int i = 0; i < sizeof(keyname) / sizeof(char *); i++)
+    {
+        if (strcmp(ndl_keyname, keyname[i]) == 0)
+        {
+            event->key.keysym.sym = i;
+            return 1;
+        }
+    }
+
+    return 0;
 }
 
-int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
-  return 0;
+int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask)
+{
+    return 0;
 }
 
-uint8_t* SDL_GetKeyState(int *numkeys) {
-  return NULL;
+uint8_t *SDL_GetKeyState(int *numkeys)
+{
+    return NULL;
 }
