0dac08647cf8e2e53412fc61ee3bbc3d614bf1d5
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-17 09:22:58 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:22:58 up  6:39,  1 user,  load average: 0.52, 0.22, 0.20

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -8,7 +8,7 @@ static uint64_t get_time()
 {
     uint32_t lo = inl(RTC_ADDR);
     uint32_t hi = inl(RTC_ADDR + 4);
-    uint64_t time = ((uint64_t)hi << 32) | lo;
+    uint64_t time = ((uint64_t)hi << 32) | (uint64_t)lo;
     return time;
 }
 
