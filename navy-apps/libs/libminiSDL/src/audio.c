a04e1524b9984cd8eae12311a74d1e1811ebf86c
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-18 02:56:09 -0700

    >  compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:56:09 up  6:45,  1 user,  load average: 0.13, 0.12, 0.10

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -25,7 +25,7 @@ void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume)
 
 SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audio_buf, uint32_t *audio_len)
 {
-    assert(0);
+    //assert(0);
     return NULL;
 }
 
