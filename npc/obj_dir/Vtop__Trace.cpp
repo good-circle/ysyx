ac91e9ab161effec2aa46a447bd444aab4e39226
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 07:27:01 -0700

    > check waveform ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:27:01 up  6:56,  1 user,  load average: 0.45, 0.30, 0.20

---
+++ npc/obj_dir/Vtop__Trace.cpp
@@ -0,0 +1,44 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Tracing implementation internals
+#include "verilated_vcd_c.h"
+#include "Vtop__Syms.h"
+
+
+void Vtop___024root__traceChgSub0(Vtop___024root* vlSelf, VerilatedVcd* tracep);
+
+void Vtop___024root__traceChgTop0(void* voidSelf, VerilatedVcd* tracep) {
+    Vtop___024root* const __restrict vlSelf = static_cast<Vtop___024root*>(voidSelf);
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    if (VL_UNLIKELY(!vlSymsp->__Vm_activity)) return;
+    // Body
+    {
+        Vtop___024root__traceChgSub0((&vlSymsp->TOP), tracep);
+    }
+}
+
+void Vtop___024root__traceChgSub0(Vtop___024root* vlSelf, VerilatedVcd* tracep) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    vluint32_t* const oldp = tracep->oldp(vlSymsp->__Vm_baseCode + 1);
+    if (false && oldp) {}  // Prevent unused
+    // Body
+    {
+        tracep->chgBit(oldp+0,(vlSelf->clk));
+        tracep->chgIData(oldp+1,(vlSelf->inst),32);
+        tracep->chgIData(oldp+2,(vlSelf->pc),32);
+        tracep->chgBit(oldp+3,(vlSelf->a));
+        tracep->chgBit(oldp+4,((IData)((0x13U == (0x707fU 
+                                                  & vlSelf->inst)))));
+    }
+}
+
+void Vtop___024root__traceCleanup(void* voidSelf, VerilatedVcd* /*unused*/) {
+    VlUnpacked<CData/*0:0*/, 1> __Vm_traceActivity;
+    Vtop___024root* const __restrict vlSelf = static_cast<Vtop___024root*>(voidSelf);
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    // Body
+    {
+        vlSymsp->__Vm_activity = false;
+        __Vm_traceActivity[0U] = 0U;
+    }
+}
---
+++ npc/obj_dir/Vtop__Trace__Slow.cpp
@@ -0,0 +1,77 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Tracing implementation internals
+#include "verilated_vcd_c.h"
+#include "Vtop__Syms.h"
+
+
+void Vtop___024root__traceInitSub0(Vtop___024root* vlSelf, VerilatedVcd* tracep) VL_ATTR_COLD;
+
+void Vtop___024root__traceInitTop(Vtop___024root* vlSelf, VerilatedVcd* tracep) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    // Body
+    {
+        Vtop___024root__traceInitSub0(vlSelf, tracep);
+    }
+}
+
+void Vtop___024root__traceInitSub0(Vtop___024root* vlSelf, VerilatedVcd* tracep) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    const int c = vlSymsp->__Vm_baseCode;
+    if (false && tracep && c) {}  // Prevent unused
+    // Body
+    {
+        tracep->declBit(c+1,"clk", false,-1);
+        tracep->declBus(c+2,"inst", false,-1, 31,0);
+        tracep->declBus(c+3,"pc", false,-1, 31,0);
+        tracep->declBit(c+4,"a", false,-1);
+        tracep->declBit(c+1,"top clk", false,-1);
+        tracep->declBus(c+2,"top inst", false,-1, 31,0);
+        tracep->declBus(c+3,"top pc", false,-1, 31,0);
+        tracep->declBit(c+4,"top a", false,-1);
+        tracep->declBit(c+5,"top addi", false,-1);
+    }
+}
+
+void Vtop___024root__traceFullTop0(void* voidSelf, VerilatedVcd* tracep) VL_ATTR_COLD;
+void Vtop___024root__traceChgTop0(void* voidSelf, VerilatedVcd* tracep);
+void Vtop___024root__traceCleanup(void* voidSelf, VerilatedVcd* /*unused*/);
+
+void Vtop___024root__traceRegister(Vtop___024root* vlSelf, VerilatedVcd* tracep) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    // Body
+    {
+        tracep->addFullCb(&Vtop___024root__traceFullTop0, vlSelf);
+        tracep->addChgCb(&Vtop___024root__traceChgTop0, vlSelf);
+        tracep->addCleanupCb(&Vtop___024root__traceCleanup, vlSelf);
+    }
+}
+
+void Vtop___024root__traceFullSub0(Vtop___024root* vlSelf, VerilatedVcd* tracep) VL_ATTR_COLD;
+
+void Vtop___024root__traceFullTop0(void* voidSelf, VerilatedVcd* tracep) {
+    Vtop___024root* const __restrict vlSelf = static_cast<Vtop___024root*>(voidSelf);
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    // Body
+    {
+        Vtop___024root__traceFullSub0((&vlSymsp->TOP), tracep);
+    }
+}
+
+void Vtop___024root__traceFullSub0(Vtop___024root* vlSelf, VerilatedVcd* tracep) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    vluint32_t* const oldp = tracep->oldp(vlSymsp->__Vm_baseCode);
+    if (false && oldp) {}  // Prevent unused
+    // Body
+    {
+        tracep->fullBit(oldp+1,(vlSelf->clk));
+        tracep->fullIData(oldp+2,(vlSelf->inst),32);
+        tracep->fullIData(oldp+3,(vlSelf->pc),32);
+        tracep->fullBit(oldp+4,(vlSelf->a));
+        tracep->fullBit(oldp+5,((IData)((0x13U == (0x707fU 
+                                                   & vlSelf->inst)))));
+    }
+}
