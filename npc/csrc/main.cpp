c2c540ee72173612bbfae5e1a32c35bfd1a04fe9
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 10:15:25 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:15:25 up  9:44,  1 user,  load average: 0.35, 0.41, 0.35

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -89,7 +89,7 @@ int main(int argc, char **argv, char **env)
         }
         top->rst = 0;
         printf("%08lx ", top->pc);
-        //top->inst = pmem_read(top->pc);
+        top->inst = pmem_read(top->pc);
         printf("%08x\n", top->inst);
 
         m_trace->dump(2 * i);
