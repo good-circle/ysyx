db9e9e24d7b6fcfb64d36ab6e6164435f6f5e1aa
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 10:10:38 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:10:38 up  9:40,  1 user,  load average: 0.86, 0.53, 0.35

--- npc/obj_dir/Vtop___024root.cpp
+++ npc/obj_dir/Vtop___024root.cpp
@@ -47,14 +47,11 @@ VL_INLINE_OPT void Vtop___024root___sequent__TOP__3(Vtop___024root* vlSelf) {
     // Body
     __Vdly__pc = vlSelf->pc;
     __Vdlyvset__top__DOT__u_regfile__DOT__rf__v0 = 0U;
-    if (VL_LIKELY(vlSelf->rst)) {
-        __Vdly__pc = (((0x6fU == (0x7fU & vlSelf->inst)) 
+    __Vdly__pc = ((IData)(vlSelf->rst) ? 0x80000000ULL
+                   : (((0x6fU == (0x7fU & vlSelf->inst)) 
                        | (IData)(vlSelf->top__DOT__jalr))
                        ? vlSelf->top__DOT__br_target
-                       : (4ULL + vlSelf->pc));
-    } else {
-        VL_FINISH_MT("vsrc/top.v", 91, "");
-    }
+                       : (4ULL + vlSelf->pc)));
     if ((1U & (~ (IData)(vlSelf->top__DOT__sd)))) {
         __Vdlyvval__top__DOT__u_regfile__DOT__rf__v0 
             = (((((- (QData)((IData)(((0x6fU == (0x7fU 
