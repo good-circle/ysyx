97d7bd22a8ae04f1c402bec0fecb19ec0b90c222
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-21 08:00:56 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:00:56 up  7:37,  1 user,  load average: 0.23, 0.18, 0.12

--- npc/csrc/disasm.cc
+++ npc/csrc/disasm.cc
@@ -20,49 +20,6 @@ static llvm::MCDisassembler *gDisassembler = nullptr;
 static llvm::MCSubtargetInfo *gSTI = nullptr;
 static llvm::MCInstPrinter *gIP = nullptr;
 
-extern "C" void init_disasm(const char *triple) {
-  llvm::InitializeAllTargetInfos();
-  llvm::InitializeAllTargetMCs();
-  llvm::InitializeAllAsmParsers();
-  llvm::InitializeAllDisassemblers();
-
-  std::string errstr;
-  std::string gTriple(triple);
-
-  llvm::MCInstrInfo *gMII = nullptr;
-  llvm::MCRegisterInfo *gMRI = nullptr;
-  auto target = llvm::TargetRegistry::lookupTarget(gTriple, errstr);
-  if (!target) {
-    llvm::errs() << "Can't find target for " << gTriple << ": " << errstr << "\n";
-    assert(0);
-  }
-
-  MCTargetOptions MCOptions;
-  gSTI = target->createMCSubtargetInfo(gTriple, "", "");
-  std::string isa = target->getName();
-  if (isa == "riscv32" || isa == "riscv64") {
-    gSTI->ApplyFeatureFlag("+m");
-    gSTI->ApplyFeatureFlag("+a");
-    gSTI->ApplyFeatureFlag("+c");
-    gSTI->ApplyFeatureFlag("+f");
-    gSTI->ApplyFeatureFlag("+d");
-  }
-  gMII = target->createMCInstrInfo();
-  gMRI = target->createMCRegInfo(gTriple);
-  auto AsmInfo = target->createMCAsmInfo(*gMRI, gTriple, MCOptions);
-#if LLVM_VERSION_MAJOR >= 13
-   auto llvmTripleTwine = Twine(triple);
-   auto llvmtriple = llvm::Triple(llvmTripleTwine);
-   auto Ctx = new llvm::MCContext(llvmtriple,AsmInfo, gMRI, nullptr);
-#else
-   auto Ctx = new llvm::MCContext(AsmInfo, gMRI, nullptr);
-#endif
-  gDisassembler = target->createMCDisassembler(*gSTI, *Ctx);
-  gIP = target->createMCInstPrinter(llvm::Triple(gTriple),
-      AsmInfo->getAssemblerDialect(), *AsmInfo, *gMII, *gMRI);
-  gIP->setPrintImmHex(true);
-  gIP->setPrintBranchImmAsAddress(true);
-}
 
 extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
   MCInst inst;
