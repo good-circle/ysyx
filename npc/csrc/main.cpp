2e79d0f440ba9f0fb6a27c4e69a8aa93eb2951aa
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 17:18:03 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:18:03 up 15 min,  1 user,  load average: 0.68, 0.87, 0.80

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -17,7 +17,7 @@ static int inst_num = 0;
 #define COLOR_NONE  "\033[0m"
 #define COLOR_GREEN "\033[1;32m"
 #define COLOR_RED "\033[1;31m"
-#define COLOR_BLUE "\033[0;31m"
+#define COLOR_BLUE "\033[0;34m"
 static int parse_args(int argc, char *argv[])
 {
     const struct option table[] = {
