409703cc0d2d4eb82ab5782cc3026d51697ac64f
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 17:48:07 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:48:07 up 45 min,  1 user,  load average: 1.27, 0.81, 0.71

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -22,4 +22,4 @@ image: $(IMAGE).elf
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: image
-	$(MAKE) -C $(NPC_HOME) sim ARGS=b IMG=$(IMAGE).bin
+	$(MAKE) -C $(NPC_HOME) sim ARGS=-b IMG=$(IMAGE).bin
