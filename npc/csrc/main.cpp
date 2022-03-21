47597bede011ce4b25dc3b18608bcc47b15e5409
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 19:12:22 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:12:22 up  1:52,  1 user,  load average: 1.26, 0.45, 0.46

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -10,7 +10,7 @@ VerilatedContext *contextp = new VerilatedContext;
 Vtop *top = new Vtop{contextp};
 svBit is_finish = 0;
 
-static u_int8_t pmem[0x8000000];
+
 static const char *img_file = NULL;
 static char *log_file = NULL;
 static int inst_num = 0;
@@ -53,32 +53,6 @@ static int parse_args(int argc, char *argv[])
     return 0;
 }
 
-void init_pmem()
-{
-    if (img_file == NULL)
-    {
-        printf("No image is given. Use the default build-in image.");
-        assert(0);
-    }
-
-    FILE *fp = fopen(img_file, "rb");
-    fseek(fp, 0, SEEK_END);
-    long size = ftell(fp);
-
-    printf("The image is %s, size = %ld\n", img_file, size);
-
-    fseek(fp, 0, SEEK_SET);
-    int ret = fread(pmem, size, 1, fp);
-    assert(ret == 1);
-
-    fclose(fp);
-}
-
-int pmem_read(unsigned long long pc)
-{
-    inst_num++;
-    return *(u_int32_t *)(pmem + pc - 0x80000000);
-}
 
 void exec(Vtop *top, VerilatedVcdC *m_trace, svBit is_finish, unsigned int n)
 {
---
+++ npc/csrc/memory.cpp
@@ -0,0 +1,28 @@
+static u_int8_t pmem[0x8000000];
+
+void init_pmem()
+{
+    if (img_file == NULL)
+    {
+        printf("No image is given. Use the default build-in image.");
+        assert(0);
+    }
+
+    FILE *fp = fopen(img_file, "rb");
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+
+    printf("The image is %s, size = %ld\n", img_file, size);
+
+    fseek(fp, 0, SEEK_SET);
+    int ret = fread(pmem, size, 1, fp);
+    assert(ret == 1);
+
+    fclose(fp);
+}
+
+int pmem_read(unsigned long long pc)
+{
+    inst_num++;
+    return *(u_int32_t *)(pmem + pc - 0x80000000);
+}
