10f090c17c7994fb1a9f83eba3e64f5e1719b376
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 01:29:19 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:29:19 up 1 day,  1:06,  1 user,  load average: 0.24, 0.26, 0.28

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -44,7 +44,7 @@ static int parse_args(int argc, char *argv[])
         {0, 0, NULL, 0},
     };
     int o;
-    while ((o = getopt_long(argc, argv, "-bl:", table, NULL)) != -1)
+    while ((o = getopt_long(argc, argv, "-bl:d", table, NULL)) != -1)
     {
         switch (o)
         {
