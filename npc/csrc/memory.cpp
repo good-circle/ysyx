b17a2c43116a6e74b78413c72122bf054776e46f
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 09:32:20 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:32:20 up 1 day,  9:09,  1 user,  load average: 0.67, 0.45, 0.25

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -95,5 +95,6 @@ extern "C" void pmem_write(long long mem_waddr, long long mem_wdata, char mem_wm
         }
 
         *(long long *)(pmem + (mem_waddr & ~0x7ull) - 0x80000000) |= (mem_wdata & real_mask);
+        printf("%lx\n", *(long long *)(pmem + (mem_waddr & ~0x7ull) - 0x80000000));
     }
 }
