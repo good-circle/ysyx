843e94374e1cc2ae5ca4c012187bf3ecc53d917f
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 01:49:33 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:49:33 up 1 day,  1:26,  1 user,  load average: 0.28, 0.28, 0.28

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -141,7 +141,6 @@ int main(int argc, char **argv, char **env)
     
     difftest_read_regs(difftest_regs);
     init_difftest(diff_so_file, img_size, difftest_regs);
-    
     top->clk = 1;
     top->rst = 0;
 
