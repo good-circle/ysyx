3bf7817fe6a93b627af122eea4080f5f0ce58985
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 01:23:46 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:23:46 up 1 day,  1:00,  1 user,  load average: 0.21, 0.31, 0.31

--- npc/csrc/difftest.cpp
+++ npc/csrc/difftest.cpp
@@ -78,10 +78,10 @@ void init_difftest(char *ref_so_file, long img_size, uint64_t *difftest_regs)
 
 int difftest_step(uint64_t *difftest_regs, uint64_t pc)
 {
-    assert(0);
     uint64_t ref_r[33];
 
     ref_difftest_exec(1);
+    assert(0);
     ref_difftest_regcpy(&ref_r, 0);
 
     bool is_different = false;
