9516b0c2c500d8f4c041613a11281f013464ca94
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 06:18:55 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:18:55 up 1 day,  5:55,  1 user,  load average: 1.42, 0.75, 0.43

--- npc/csrc/disasm.cc
+++ npc/csrc/disasm.cc
@@ -72,7 +72,6 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
     MCInst inst;
     llvm::ArrayRef<uint8_t> arr(code, nbyte);
     uint64_t dummy_size = 0;
-    assert(0);
     gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
 
     std::string s;
