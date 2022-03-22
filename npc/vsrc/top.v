9d7d15bb53a5fcb2fd133ad66d75587021a61551
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 08:04:33 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:04:33 up 1 day,  7:41,  1 user,  load average: 0.28, 0.41, 0.29

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -149,7 +149,7 @@ wire [63:0] mem_waddr;
 wire [63:0] mem_wdata;
 wire [7:0] mem_wmask;
 wire mem_write;
-assign mem_raddr = 64'b0;
+assign mem_raddr = 64'h0000000080000000;
 assign mem_read = 1'b1;
 assign mem_waddr = 64'b0;
 assign mem_wdata = 64'b0;
