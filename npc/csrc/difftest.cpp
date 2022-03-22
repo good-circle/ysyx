e7df7201edb7eb420daa7aee9b8e13c1488e815e
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 01:35:40 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:35:40 up 1 day,  1:12,  1 user,  load average: 0.17, 0.35, 0.33

--- npc/csrc/difftest.cpp
+++ npc/csrc/difftest.cpp
@@ -8,7 +8,7 @@ typedef uint64_t paddr_t;
 typedef uint64_t vaddr_t;
 extern Vtop *top;
 extern void isa_reg_display();
-
+extern const char *img_file;
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
@@ -72,9 +72,7 @@ void init_difftest(char *ref_so_file, long img_size, uint64_t *difftest_regs)
     assert(ref_difftest_init);
 
     ref_difftest_init();
-    ref_difftest_memcpy(0x80000000, 0, img_size, 1);
-        assert(0);
-
+    ref_difftest_memcpy(0x80000000, img_file, img_size, 1);
     ref_difftest_regcpy(difftest_regs, 1);
 }
 
