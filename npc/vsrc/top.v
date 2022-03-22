377ff828df1ffd43fa89429a3b9669e890603961
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 08:01:29 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:01:29 up 1 day,  7:38,  1 user,  load average: 0.53, 0.37, 0.24

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -165,7 +165,7 @@ wire [63:0] mem_rdata;
 always @(*) begin
   pmem_read(mem_raddr, mem_rdata, mem_read);
   pmem_write(mem_waddr, mem_wdata, mem_wmask, mem_write);
-  //$display("%lx",mem_rdata);
+  $display("%lx",mem_rdata);
 end
 
 export "DPI-C" task finish;
@@ -174,7 +174,6 @@ task finish;
     is_finish = ebreak;
 endtask
 
-//assign halt = rf_rdata1;
-assign halt = mem_rdata;
+assign halt = rf_rdata1;
 
 endmodule
