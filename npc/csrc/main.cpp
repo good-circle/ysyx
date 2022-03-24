1eeed0004ea15e44a0e9b67f137c73c4c48b1cf7
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-24 06:46:54 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:46:54 up  4:50,  1 user,  load average: 0.21, 0.16, 0.23

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -171,7 +171,7 @@ int main(int argc, char **argv, char **env)
     top->clk = 1;
     top->rst = 0;
     
-    svSetScope(svGetScopeFromName("blackbox"));
+    svSetScope(svGetScopeFromName("BLACKBOX.blackbox"));
 
     sdb_mainloop();
 
