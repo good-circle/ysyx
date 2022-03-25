af28f20249e4fa1fa24f85ac849dfc34a6939016
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-25 04:39:30 -0700

    > generate verilog ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:39:30 up 14:29,  1 user,  load average: 0.44, 0.31, 0.28

--- chisel-playground/playground/src/cpu/ALU.scala
+++ chisel-playground/playground/src/cpu/ALU.scala
@@ -19,7 +19,7 @@ class ALU extends Module {
   srl := (aluop === alu_srl) && rv64
   sra := (aluop === alu_sra) && rv64
 
-  val src1 = Muxcase(io.src1, Array(
+  val src1 = MuxCase(io.src1, Array(
     srl -> Cat(Fill(32, 0.U), io.src1(31, 0))
     sra -> Cat(Fill(32, io.src1(31)), io.src1(31, 0))
   ))
