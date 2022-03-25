80aa5bc28a7d2166f62e80335e0655ec09fa07ce
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-25 08:10:34 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:10:34 up 31 min,  1 user,  load average: 0.36, 0.26, 0.16

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -45,6 +45,7 @@ void difftest_skip_dut(int nr_ref, int nr_dut) {
 }
 
 void init_difftest(char *ref_so_file, long img_size, int port) {
+    printf("port is %d\n", port);
   assert(ref_so_file != NULL);
 
   void *handle;
