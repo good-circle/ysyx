bfd032ad2c6a0087500bcc1bb4645059473895a4
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-25 02:01:54 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:01:54 up 13:23,  1 user,  load average: 0.79, 0.57, 0.37

--- npc/csrc/chisel/reg.cpp
+++ npc/csrc/chisel/reg.cpp
@@ -48,6 +48,7 @@ void difftest_read_regs(uint64_t *difftest_regs)
     difftest_regs[0] = 0;
     for (int i = 1; i < 32; i++)
     {
+        printf("%d\n", i);
         difftest_regs[i] = cpu_gpr[i];
     }
     difftest_regs[32] = top->io_pc;
