fb702b583439635a4539087970076ff24987216c
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-24 22:08:01 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:08:01 up 11:58,  1 user,  load average: 0.60, 0.54, 0.53

--- npc/csrc/chisel/main.cpp
+++ npc/csrc/chisel/main.cpp
@@ -162,9 +162,9 @@ int main(int argc, char **argv, char **env)
     init_regex();
 
     reset_npc(10);
-
+    assert(0);
     difftest_read_regs(difftest_regs);
-        assert(0);
+    
     init_difftest(diff_so_file, img_size, difftest_regs);
 #ifdef ITRACE
     init_disasm("riscv64-pc-linux-gnu");
