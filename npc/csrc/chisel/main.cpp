032d52900a398b3b02457fdb3494ec6be274975a
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-24 22:33:21 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:33:21 up 12:23,  1 user,  load average: 0.35, 0.35, 0.34

--- npc/csrc/chisel/main.cpp
+++ npc/csrc/chisel/main.cpp
@@ -131,8 +131,8 @@ void npc_exec(unsigned int n)
 
         if (!is_finish && difftest_step(difftest_regs, last_pc) != 0)
         {
-            //is_finish = 1;
-            //break;
+            is_finish = 1;
+            break;
         }
     }
 
