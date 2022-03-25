8a9acd0c5c1077a38c6154a4e837ef29692876c8
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-25 02:01:03 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:01:03 up 13:22,  1 user,  load average: 0.88, 0.55, 0.35

--- npc/csrc/chisel/main.cpp
+++ npc/csrc/chisel/main.cpp
@@ -89,7 +89,7 @@ void npc_exec(unsigned int n)
     while (!is_finish && n > 0)
     {
         u_int64_t last_pc = top->io_pc;
-        //printf("%08lx ", top->io_pc);
+        printf("%08lx ", top->io_pc);
         //top->inst = inst_fetch(top->io_pc);
         //printf("%08x\n", top->inst);
 
