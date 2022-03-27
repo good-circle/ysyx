9c6a5ad2c21b822b1c1e4437acdb1822123b1137
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-27 01:30:20 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:30:20 up  1:24,  1 user,  load average: 0.84, 0.81, 0.55

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -6,17 +6,6 @@ int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args)
 {
-        assert(0);
-    uintptr_t argc = *args;
-    printf("argc: %d\n", argc);
-    assert(0);
-    args = (uintptr_t *)(args + 1);
-    char **argv = (char **)(args);
 
-    args = (uintptr_t *)(args + argc + 1);
-    char **envp = (char **)(args);
-
-    environ = envp;
-    exit(main(argc, argv, envp));
     assert(0);
 }
