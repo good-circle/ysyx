fcae0d4268197b90937a8f54ba8a5ac4629637d7
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-28 09:24:41 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:24:41 up 14:11,  1 user,  load average: 0.64, 0.57, 0.50

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -68,7 +68,7 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg)
 
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[])
 {
-    printf("%s\n", filename);
+    printf("position of argv is %p, envp is %p\n", argv, envp);
     uintptr_t entry = loader(pcb, filename);
     Log("uload: entry = %p", entry);
 
@@ -78,7 +78,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     pcb->cp = ucontext(NULL, kstack, (void (*)())entry);
 
     void *ustack = new_page(8) + 8 * PGSIZE;
-    printf("position of ustack is %x\n", ustack);
     char *envp_buf[128];
     char *argv_buf[128];
     uintptr_t envp_num = 0;
