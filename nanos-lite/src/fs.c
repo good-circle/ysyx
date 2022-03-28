b55b16f55c24da2a80741b33af30a4c7bf016624
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-28 09:09:55 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:09:55 up 13:57,  1 user,  load average: 0.99, 0.54, 0.40

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -11,7 +11,7 @@ size_t events_read(void *buf, size_t offset, size_t len);
 size_t dispinfo_read(void *buf, size_t offset, size_t len);
 size_t fb_write(const void *buf, size_t offset, size_t len);
 
-#define STRACE
+//#define STRACE
 
 typedef struct
 {
