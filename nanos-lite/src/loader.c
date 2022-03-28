90f6d26669c8f01203fec997f69e5277e10dc171
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-28 09:13:50 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:13:50 up 14:01,  1 user,  load average: 0.68, 0.65, 0.48

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,14 +15,13 @@ void *new_page(size_t nr_page);
 
 static uintptr_t loader(PCB *pcb, const char *filename)
 {
-                printf("filename: %s\n", filename);
     Elf_Ehdr *ehdr = malloc(sizeof(Elf_Ehdr));
     Elf_Phdr *phdr = malloc(sizeof(Elf_Phdr));
 
     /* read elf from ramdisk */
     int fd = fs_open(filename, 0, 0);
     fs_read(fd, ehdr, sizeof(Elf_Ehdr));
-
+printf("filename: %s\n", filename);
     /* assert if not elf file */
     //! pay attention: this is uint32_t not size_t
     assert(*(uint32_t *)ehdr->e_ident == 0x464c457f);
