bfaa3d208bdb18683d77eba363af86f9da00b0b3
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-29 10:59:57 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:59:57 up  1:53,  1 user,  load average: 0.59, 0.24, 0.20

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -33,13 +33,12 @@ void init_proc()
 
     //char *skip_arg[] = {"/bin/pal","--skip", NULL};
     //char *exec_arg[] = {"/bin/menu"};
-    //char *null_arg[] = {NULL};
+    char *null_arg[] = {NULL};
 
     // load program here
     //context_kload(&pcb[0], hello_fun, (void *)12345678);
     //context_uload(&pcb[0], "/bin/pal", skip_arg, null_arg);
-    //context_uload(&pcb[0], "/bin/nterm", null_arg, null_arg);
-    naive_uload(NULL, "/bin/pal");
+    context_uload(&pcb[0], "/bin/pal", null_arg, null_arg);
 }
 
 Context *schedule(Context *prev)
