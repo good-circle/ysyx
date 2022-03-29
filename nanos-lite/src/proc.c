1770c9c3e0fb6a83b928601abd62749d0bc44cc4
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-29 09:19:31 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:19:31 up 13 min,  1 user,  load average: 0.83, 0.64, 0.36

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -36,9 +36,9 @@ void init_proc()
     char *null_arg[] = {NULL};
 
     // load program here
-    context_kload(&pcb[0], hello_fun, (void *)12345678);
+    //context_kload(&pcb[0], hello_fun, (void *)12345678);
     //context_uload(&pcb[0], "/bin/pal", skip_arg, null_arg);
-    context_uload(&pcb[1], "/bin/nterm", null_arg, null_arg);
+    context_uload(&pcb[0], "/bin/nterm", null_arg, null_arg);
 }
 
 Context *schedule(Context *prev)
@@ -46,8 +46,8 @@ Context *schedule(Context *prev)
     // save the context pointer
     current->cp = prev;
 
-    current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-    //current = &pcb[0];
+    //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+    current = &pcb[0];
 
     // then return the new context
     return current->cp;
