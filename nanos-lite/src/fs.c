a3024cf652925bf335c76c2b634f7a8fa136d659
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 15:19:25 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:19:25 up  5:27,  1 user,  load average: 0.85, 0.65, 0.57

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -85,6 +85,7 @@ int fs_open(const char *pathname, int flags, int mode)
 
 size_t fs_read(int fd, void *buf, size_t len)
 {
+    printf("buf addr = %p\n", buf);
     if (file_table[fd].read != NULL)
     {
         len = file_table[fd].read(buf, file_table[fd].open_offset, len);
--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -12,7 +12,6 @@ extern uint8_t ramdisk_end;
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
-  printf("buf addr = %p\n", buf);
   memcpy(buf, &ramdisk_start + offset, len);
   return len;
 }
