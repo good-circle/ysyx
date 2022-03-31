cb99439d8eb197969a0894e67a224ec66f68dd59
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 14:04:04 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:04:04 up  4:12,  1 user,  load average: 0.55, 0.49, 0.26

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -8,9 +8,9 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type)
     vaddr_t vpn1 = (vaddr << 34) >> 55;
     vaddr_t vpn2 = (vaddr << 25) >> 55;
     vaddr_t offset = (vaddr << 52) >> 52;
-    printf("vaddr %lx, vpn0 %lx, vpn1 %lx, vpn2 %lx ", vaddr, vpn0, vpn1, vpn2);
+    printf("vaddr %lx ", vaddr);
     word_t base_addr = (cpu.csr[SATP] << 20) >> 8;
-    printf("base_addr = %lx ", (cpu.csr[SATP] << 20) >> 8);
+    printf("1: %lx ",base_addr + vpn2 * sizeof(word_t));
     word_t first_level_pgdir = paddr_read(base_addr + vpn2 * sizeof(word_t), sizeof(word_t));
     word_t second_level_pgdir = paddr_read(((first_level_pgdir >> 10) << 12) + vpn1 * sizeof(word_t), sizeof(word_t));
     word_t last_level_pgdir = paddr_read(((second_level_pgdir >> 10) << 12) + vpn0 * sizeof(word_t), sizeof(word_t));
