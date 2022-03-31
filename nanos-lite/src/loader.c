3eb8842f49ad6d5f85136f20df3c12b0a4f1f46f
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 14:22:58 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:22:58 up  4:31,  1 user,  load average: 0.88, 0.58, 0.43

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -60,7 +60,6 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg)
 
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[])
 {
-    assert(0);
     void *ustack = new_page(8) + 8 * PGSIZE;
     char *envp_buf[128];
     char *argv_buf[128];
