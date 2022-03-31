8729f5c48d2d02a17a917d97129ec7e322f143ff
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 15:43:27 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:43:27 up  5:48,  1 user,  load average: 0.79, 0.54, 0.53

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -34,7 +34,7 @@ printf("ehdr %p, phdr %p\n", ehdr, phdr);
         if (phdr->p_type == PT_LOAD)
         {
             fs_lseek(fd, phdr->p_offset, SEEK_SET);
-                        assert(0);
+            printf("%x\n", phdr->p_vaddr);
             fs_read(fd, (void *)phdr->p_vaddr, phdr->p_filesz);
             /* padding filesz ~ memsz zero */
             memset((void *)(phdr->p_vaddr + phdr->p_filesz), 0, phdr->p_memsz - phdr->p_filesz);
