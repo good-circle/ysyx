bd4290b5a43848abbb23d68e154c2ffb80c56af8
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 14:06:07 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:06:07 up  4:14,  1 user,  load average: 0.65, 0.52, 0.30

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -8,16 +8,19 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type)
     vaddr_t vpn1 = (vaddr << 34) >> 55;
     vaddr_t vpn2 = (vaddr << 25) >> 55;
     vaddr_t offset = (vaddr << 52) >> 52;
-    printf("vaddr %lx ", vaddr);
     word_t base_addr = (cpu.csr[SATP] << 20) >> 8;
-    printf("1: %lx ",base_addr + vpn2 * sizeof(word_t));
+
     word_t first_level_pgdir = paddr_read(base_addr + vpn2 * sizeof(word_t), sizeof(word_t));
     assert((first_level_pgdir & 0x1) == 1);
+
     word_t second_level_pgdir = paddr_read(((first_level_pgdir >> 10) << 12) + vpn1 * sizeof(word_t), sizeof(word_t));
+    assert((second_level_pgdir & 0x1) == 1);
+
     word_t last_level_pgdir = paddr_read(((second_level_pgdir >> 10) << 12) + vpn0 * sizeof(word_t), sizeof(word_t));
+    assert((last_level_pgdir & 0x1) == 1);
 
     word_t paddr = ((last_level_pgdir >> 10) << 12) | offset;
-    printf("paddr = %lx\n", paddr);
     assert(paddr == vaddr);
+    
     return paddr;
 }
