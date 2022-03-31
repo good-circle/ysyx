cb009d9b13d72ff1389ac6c9372740c87d8b2ff5
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 15:20:04 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:20:04 up  5:28,  1 user,  load average: 0.54, 0.60, 0.55

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -34,6 +34,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
         if (phdr->p_type == PT_LOAD)
         {
             fs_lseek(fd, phdr->p_offset, SEEK_SET);
+            printf("buf addr = %p\n", (void *)phdr->p_vaddr);
             fs_read(fd, (void *)phdr->p_vaddr, phdr->p_filesz);
             /* padding filesz ~ memsz zero */
             memset((void *)(phdr->p_vaddr + phdr->p_filesz), 0, phdr->p_memsz - phdr->p_filesz);
