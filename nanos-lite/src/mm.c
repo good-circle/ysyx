0689cc9870d907e58d812b64a5aa22426c3bbf2a
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-01 08:12:00 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.11.0-46-generic #51-Ubuntu SMP Thu Jan 6 22:14:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:12:00 up 11:42,  1 user,  load average: 0.58, 0.28, 0.19

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -31,15 +31,13 @@ int mm_brk(uintptr_t brk)
     uintptr_t current_ppn = current->max_brk >> 12;
     uintptr_t new_ppn = brk >> 12;
 
-    printf("old: %x, new: %x\n", current_ppn << 12, new_ppn << 12);
-
     if (brk >= current->max_brk)
     {
         void *new = new_page(new_ppn - current_ppn);
-        for (int i = 0; i < new_ppn -current_ppn; i++)
+        for (int i = 0; i < new_ppn - current_ppn; i++)
         {
 
-            map(&current->as, (void *)(current->max_brk + i * PGSIZE), (void *)(new + i * PGSIZE), 0);
+            map(&current->as, (void *)(current->max_brk + i * PGSIZE), (void *)(new + i *PGSIZE), 0);
         }
 
         current->max_brk = new_ppn << 12;
