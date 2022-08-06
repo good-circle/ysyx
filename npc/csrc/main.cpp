dfd42617ad7534a86134019e58505b3adf9dc87a
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-06 04:38:46 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:38:46 up 13:55,  1 user,  load average: 0.16, 0.12, 0.16

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -190,7 +190,7 @@ int main(int argc, char **argv, char **env)
     */
     top->reset = 0;
 
-    svSetScope(svGetScopeFromName("Commit"));
+    svSetScope(svGetScopeFromName("Simtop.Commit"));
 
     sdb_mainloop();
 
