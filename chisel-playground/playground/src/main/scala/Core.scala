b8aa081a9b27caadf100c911908ce6ebf1988b83
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 23:11:48 -0700

    > generate verilog ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:11:48 up 11:05,  1 user,  load average: 0.27, 0.29, 0.25

--- chisel-playground/playground/src/main/scala/Core.scala
+++ chisel-playground/playground/src/main/scala/Core.scala
@@ -27,43 +27,4 @@ class Core extends Module {
   exu.io.dmem <> io.dmem
   Connect(exu.io.out, wbu.io.in, true.B, false.B)
 
-  /* ----- Difftest ------------------------------ */
-
-  val dt_ic = Module(new DifftestInstrCommit)
-  dt_ic.io.clock    := clock
-  dt_ic.io.coreid   := 0.U
-  dt_ic.io.index    := 0.U
-  dt_ic.io.valid    := RegNext(wbu.io.commit.valid)
-  dt_ic.io.pc       := RegNext(wbu.io.commit.pc)
-  dt_ic.io.instr    := RegNext(wbu.io.commit.inst)
-  dt_ic.io.special  := 0.U
-  dt_ic.io.skip     := RegNext(wbu.io.commit.inst === "h0000007b".U || wbu.io.commit.mcycle || wbu.io.commit.is_clint) 
-  dt_ic.io.isRVC    := false.B
-  dt_ic.io.scFailed := false.B
-  dt_ic.io.wen      := RegNext(wbu.io.commit.wen)
-  dt_ic.io.wdata    := RegNext(wbu.io.commit.wdata)
-  dt_ic.io.wdest    := RegNext(wbu.io.commit.waddr)
-
-  val cycle_cnt = RegInit(0.U(64.W))
-  val instr_cnt = RegInit(0.U(64.W))
-
-  cycle_cnt := cycle_cnt + 1.U
-  instr_cnt := instr_cnt + 1.U
-
-  val rf_a0 = WireInit(0.U(64.W))
-  BoringUtils.addSink(rf_a0, "rf_a0")
-
-  val dt_te = Module(new DifftestTrapEvent)
-  dt_te.io.clock    := clock
-  dt_te.io.coreid   := 0.U
-  dt_te.io.valid    := (wbu.io.commit.inst === "h0000006b".U)
-  dt_te.io.code     := rf_a0(2, 0)
-  dt_te.io.pc       := wbu.io.commit.pc
-  dt_te.io.cycleCnt := cycle_cnt
-  dt_te.io.instrCnt := instr_cnt
-
-  // PUTCH
-  when (dt_ic.io.valid && dt_ic.io.instr === PUTCH) {
-    printf("%c", rf_a0(7, 0))
-  }
 }
