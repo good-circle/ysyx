cec175f81ae77e90601c8efa9316e09d0174d529
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-06 05:20:51 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:20:51 up 14:37,  1 user,  load average: 0.42, 0.19, 0.12

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -190,7 +190,7 @@ int main(int argc, char **argv, char **env)
     */
     top->reset = 0;
 
-    svSetScope(svGetScopeFromName("TOP.dut"));
+    svSetScope(svGetScopeFromName("TOP.123"));
 
     sdb_mainloop();
 
