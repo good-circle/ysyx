83a5c9962b21d8203b3560b3f4cc7d14e198f3fa
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 04:39:00 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:39:00 up 19:54,  1 user,  load average: 0.17, 0.12, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -92,6 +92,7 @@ void npc_exec(unsigned int n)
     struct timeval begin;
     struct timeval end;
     gettimeofday(&begin, NULL);
+    n = 50;
     while (!is_finish && n > 0)
     {
         cycle_num++;
