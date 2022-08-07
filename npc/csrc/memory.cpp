11e6d1020153508342b136477d0176cbb44cc9ce
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-06 21:43:59 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:43:59 up 17:18,  1 user,  load average: 0.23, 0.21, 0.18

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -47,9 +47,10 @@ extern "C" uint64_t pmem_read(long long mem_raddr, bool mem_read)
 {
     if(mem_read)
     printf("mem_raddr: %llx\n", mem_raddr);
-    if(mem_raddr >= 0x80000000 || !mem_read) return 0;
+    assert(mem_raddr >= 0x80000000 || !mem_read);
     if (mem_read)
     {
+        printf("111 %lx 111\n", *(long long *)(pmem + (mem_raddr & ~0x7ull) - 0x80000000));
         return *(long long *)(pmem + (mem_raddr & ~0x7ull) - 0x80000000);
     }
     return 0;
@@ -59,7 +60,7 @@ extern "C" void pmem_write(long long mem_waddr, long long mem_wdata, char mem_wm
 {
     if(mem_write)
     printf("mem_waddr: %llx\n", mem_waddr);
-    if (mem_waddr >= 0x80000000 || !mem_write) return;
+    assert(mem_waddr >= 0x80000000 || !mem_write);
     if (mem_write)
     {
         unsigned long long real_mask = 0;
