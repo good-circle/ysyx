8e8beacc6c3bfe14b8ad38c7d97a07f90e141eef
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 04:51:45 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:51:45 up 20:07,  1 user,  load average: 0.30, 0.22, 0.14

--- npc/csrc/difftest.cpp
+++ npc/csrc/difftest.cpp
@@ -91,14 +91,14 @@ int difftest_step(uint64_t *difftest_regs, uint64_t pc)
     if (difftest_regs[32] != ref_r[32])
     {
         printf("pc is different after executing instruction at pc 0x%08lx, right= 0x%lx, wrong = 0x%lx\n", pc, ref_r[32], difftest_regs[32]);
-        //is_different = true;
+        is_different = true;
     }
     for (int i = 0; i < 32; i++)
     {
         if (difftest_regs[i] != ref_r[i])
         {
             printf("reg %s(%d) is different after executing instruction at pc 0x%08lx, right= 0x%lx, wrong = 0x%lx\n", regs[i], i, pc, ref_r[i], difftest_regs[i]);
-            //is_different = true;
+            is_different = true;
         }
     }
 
