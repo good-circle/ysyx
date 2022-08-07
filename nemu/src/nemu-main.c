e0e181dc4c4ffaa21299708ffeb5c8a7cbb9cdbc
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-06 21:01:13 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:01:13 up 16:36,  1 user,  load average: 0.09, 0.16, 0.11

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -6,10 +6,8 @@ void engine_start();
 int is_exit_status_bad();
 
 int main(int argc, char *argv[]) {
-    assert(0);
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
-printf("11111\n\n");
   am_init_monitor();
 #else
   init_monitor(argc, argv);
