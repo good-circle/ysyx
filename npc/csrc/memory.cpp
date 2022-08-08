9f4e03c28396ca6aeaa810854df2da620326e015
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-08 00:35:31 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:35:30 up 1 day,  3:48,  1 user,  load average: 0.10, 0.13, 0.09

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -77,9 +77,9 @@ uint32_t lo = 0;
 uint32_t hi = 0;
 extern "C" uint64_t pmem_read(long long mem_raddr, bool mem_read)
 {
-    if (mem_read)
+    if (mem_read && (mem_raddr == 0xa0000048))
     {
-        // printf("mem_raddr: %llx\n", mem_raddr);
+        printf("mem_raddr: %llx\n", mem_raddr);
     }
 
     // assert(mem_raddr >= CONFIG_MBASE || !mem_read);
@@ -101,7 +101,6 @@ extern "C" uint64_t pmem_read(long long mem_raddr, bool mem_read)
         }
         if (mem_raddr = 0xa0000052)
         {
-            assert(0);
             return hi;
         }
     }
