5dba08d37be84e0a63152d75e290a348427d95bb
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 23:27:59 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:27:59 up 1 day,  2:40,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -170,7 +170,7 @@ void npc_exec(unsigned int n)
 
                 if(is_mmio)
                 {
-                    difftest_skip_ref();
+                    ref_difftest_regcpy(difftest_regs, 1);
                 }
                 if (!is_finish && difftest_step(difftest_regs, cpu_pc) != 0)
                 {
