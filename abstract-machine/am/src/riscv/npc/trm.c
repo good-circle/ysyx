e07611a237b2460e862e2882aa4a2ae53723b7c8
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 20:04:16 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:04:16 up 1 day, 20 min,  1 user,  load average: 0.04, 0.04, 0.00

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -16,7 +16,7 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
-  outb(SERIAL_PORT, ch);
+  //outb(SERIAL_PORT, ch);
 }
 
 void halt(int code) {
