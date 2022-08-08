bab5241c97bd8576865a77e148183b7b41217b3d
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 23:28:26 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:28:26 up 1 day,  2:40,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -38,7 +38,7 @@ extern void init_difftest(char *ref_so_file, long img_size, u_int64_t *difftest_
 void reset_npc(uint n);
 extern void difftest_read_regs(u_int64_t *difftest_regs);
 extern int difftest_step(u_int64_t *difftest_regs, u_int64_t pc);
-extern void difftest_skip_ref();
+extern void (*ref_difftest_regcpy)(void *dut, bool direction);
 extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
 extern "C" void init_disasm(const char *triple);
 
