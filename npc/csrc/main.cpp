01bf78278181b1e0a4b3539f1711fedf06359386
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 22:49:12 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:49:12 up 1 day,  2:06,  1 user,  load average: 0.09, 0.22, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -162,6 +162,7 @@ void npc_exec(unsigned int n)
                 is_finish = export_finish();
                 is_mmio = export_mmio();
                 printf("is_mmio : %d\n", is_mmio);
+                printf("cpu_pc : %lx\n", cpu_pc);
                 assert(is_mmio == 0);
 
                 // printf("\n is_finish = %d\n", is_finish);
