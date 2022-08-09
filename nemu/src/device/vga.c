3dbf29ff137b6dd4eef2e0d1a5d98cd82f866696
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-08 20:51:15 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:51:15 up  1:30,  1 user,  load average: 0.01, 0.04, 0.04

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -46,10 +46,10 @@ static void init_screen()
 
 static inline void update_screen()
 {
-    SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(uint32_t));
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
-    SDL_RenderPresent(renderer);
+    //SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(uint32_t));
+    //SDL_RenderClear(renderer);
+    //SDL_RenderCopy(renderer, texture, NULL, NULL);
+    //SDL_RenderPresent(renderer);
 }
 #else
 static void init_screen()
@@ -87,5 +87,5 @@ void init_vga()
     vmem = new_space(screen_size());
     add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);
     IFDEF(CONFIG_VGA_SHOW_SCREEN, init_screen());
-    //IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
+    IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
 }
