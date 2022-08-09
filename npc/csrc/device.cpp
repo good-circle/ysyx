9395a2d2bcc38bf37112ae10fb34258a0ab78e0b
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 00:13:22 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:13:22 up  4:52,  1 user,  load average: 0.00, 0.00, 0.01

--- npc/csrc/device.cpp
+++ npc/csrc/device.cpp
@@ -10,8 +10,24 @@ static SDL_Texture *texture = NULL;
 static void *vmem = NULL;
 static uint32_t *vgactl_port_base = NULL;
 
+static uint32_t screen_width()
+{
+    return SCREEN_W;
+}
+
+static uint32_t screen_height()
+{
+    return SCREEN_H;
+}
+
+static uint32_t screen_size()
+{
+    return screen_width() * screen_height() * sizeof(uint32_t);
+}
+
 static void init_screen()
 {
+    memset(vmem, 0, screen_size());
     SDL_Window *window = NULL;
     char title[128];
     sprintf(title, "riscv64-NPC");
