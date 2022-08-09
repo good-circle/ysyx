5d01b7630d82833476e162339cb46ff0c66e61ae
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-08 20:51:41 -0700

    > run NEMU ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:51:41 up  1:31,  1 user,  load average: 0.12, 0.06, 0.05

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -87,5 +87,5 @@ void init_vga()
     //vmem = new_space(screen_size());
     add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);
     IFDEF(CONFIG_VGA_SHOW_SCREEN, init_screen());
-    IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
+    //IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
 }
