fb0d58946fab248a47566ef1163efc13c0d98362
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-10 18:59:23 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:59:23 up 15 min,  1 user,  load average: 0.05, 0.09, 0.08

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -12,7 +12,7 @@ extern u_int8_t pmem[CONFIG_MSIZE];
 extern const char *img_file;
 extern int inst_num;
 extern int vga_size();
-extern 
+extern uint64_t cpu_pc;
 
 long init_pmem()
 {
@@ -117,7 +117,7 @@ extern "C" uint64_t pmem_read(long long mem_raddr, bool mem_read)
             return vga_size();
         }
 
-        printf("mem_raddr: %llx\n", mem_raddr);
+        printf("pc: %llx mem_raddr: %llx\n", cpu_pc, mem_raddr);
 
         // assert(mem_raddr != 0xa0000100);
     }
