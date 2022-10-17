941961f87217fbc6797134289cdebc0effaea546
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-17 05:07:12 -0700

    > generate verilog ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:07:12 up 4 days, 22:58,  1 user,  load average: 0.03, 0.11, 0.20

--- chisel-playground/playground/src/main/scala/MyElaborate.scala
+++ chisel-playground/playground/src/main/scala/MyElaborate.scala
@@ -2,6 +2,5 @@ object MyElaborate extends App {
   (new chisel3.stage.ChiselStage).execute(args, Seq(
     chisel3.stage.ChiselGeneratorAnnotation(() => new MySimTop()),
     firrtl.stage.RunFirrtlTransformAnnotation(new AddModulePrefix()),
-    ModulePrefixAnnotation("ysyx_040091_")
   ))
 }
