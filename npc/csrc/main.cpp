30f5d7e58ac23df03c2b21f08f198a0b33706bbe
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-20 20:02:59 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:02:59 up 8 days, 13:54,  1 user,  load average: 1.54, 0.64, 0.28

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -151,7 +151,7 @@ void npc_exec(unsigned int n)
     while (!is_finish && n > 0)
     {
         cycle_num++;
-        printf("%d\n", cycle_num);
+        printf("%d", cycle_num);
         printf("%lx\n", top->io_pc)
         // printf("%08lx \n", top->io_pc);
         // top->inst = inst_fetch(top->io_pc);
