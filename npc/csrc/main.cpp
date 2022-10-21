0e9635cd3c188fb61d621a725a5b8a2c3e006872
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-20 21:40:09 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:40:09 up 8 days, 15:31,  1 user,  load average: 0.16, 0.16, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -205,6 +205,7 @@ void npc_exec(unsigned int n)
         }
 
         is_finish = (top->io_commit_0_inst == 0x00100073) || (top->io_commit_1_inst == 0x00100073);
+        printf("%d\n", is_finish);
 
 
         if (commit_0)
