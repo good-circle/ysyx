05dc04bf8eadc4ec851189e5c597b89bdcf54918
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-20 19:44:05 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:44:05 up 8 days, 13:35,  1 user,  load average: 0.10, 0.09, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -268,11 +268,11 @@ int main(int argc, char **argv, char **env)
     init_regex();
 
     reset_npc(10);
-    assert(0);
+
     difftest_read_regs(difftest_regs);
 
     init_difftest(diff_so_file, img_size, difftest_regs);
-
+    assert(0);
     init_device();
 
     connect_wire(mem_ptr, top);
