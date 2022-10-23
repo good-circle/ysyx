b44882c2fc8371399ebddd6b8b08e6918c667420
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-22 23:30:54 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:30:54 up 33 min,  1 user,  load average: 0.16, 0.18, 0.14

--- npc/csrc/device.cpp
+++ npc/csrc/device.cpp
@@ -86,4 +86,13 @@ void device_update()
     }
     last = cur;
     vga_update_screen();
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT)
+        {
+            exit();
+        }
+    }
 }
