9570b72c625d4318b3f7ae9ba66500ed934c4f06
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-23 05:40:47 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:40:47 up  4:01,  1 user,  load average: 0.53, 0.60, 0.53

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -5,7 +5,7 @@
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 {
-    printf("addr = %x, n = %ld\n", addr, n);
+    //printf("addr = %x, n = %ld\n", addr, n);
     if (direction == DIFFTEST_TO_DUT)
     {
         memcpy(buf, guest_to_host(addr), n);
