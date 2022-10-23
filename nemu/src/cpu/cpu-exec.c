5bbdeca3d13aa04e998309c91d958d6780ea3da4
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-23 07:16:34 -0700

    > compile NEMU ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:16:34 up  5:37,  1 user,  load average: 0.63, 0.37, 0.30

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -148,10 +148,10 @@ static void execute(uint64_t n)
 {
     Decode s;
     //int i = 0;
-    printf("n = %ld\n", n);
+    //printf("n = %ld\n", n);
     for (; n > 0; n--)
     {
-        //printf("nemu cpu-exec.c execute cpu.pc = %lx\n", cpu.pc);
+        printf("nemu cpu-exec.c execute cpu.pc = %lx\n", cpu.pc);
         exec_once(&s, cpu.pc);
         //printf("%d\n", i++);
         //printf("%lx\n", cpu.pc);
