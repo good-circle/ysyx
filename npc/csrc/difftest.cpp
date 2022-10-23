72e5e1e596c8b58bbef849dd70dca231e3aed68c
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-23 05:57:51 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:57:51 up  4:18,  1 user,  load average: 0.40, 0.45, 0.50

--- npc/csrc/difftest.cpp
+++ npc/csrc/difftest.cpp
@@ -86,11 +86,18 @@ int difftest_step(uint64_t *difftest_regs, uint64_t pc, int num, bool skip)
     }
 
     uint64_t ref_r[33];
-    //printf("num=%d\n", num);
+    // printf("num=%d\n", num);
     ref_difftest_exec(num);
     ref_difftest_regcpy(&ref_r, 0);
 
     printf("right_pc = %lx\n", ref_r[32]);
+    for (int i = 0; i < 32; i++)
+    {
+        for (int i = 0; i < 32; i++)
+        {
+            printf("0x%lx\t\t\n", ref_r[i]);
+        }
+    }
 
     bool is_different = false;
 
