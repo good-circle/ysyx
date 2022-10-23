b41dd9e5a1487e5b7838ec57f33fcd2848b63a3c
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-23 07:17:14 -0700

    > sim RTL ysyx_220091 冯浩原 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:17:14 up  5:37,  1 user,  load average: 0.58, 0.39, 0.31

--- npc/csrc/difftest.cpp
+++ npc/csrc/difftest.cpp
@@ -82,7 +82,7 @@ int difftest_step(uint64_t *difftest_regs, uint64_t pc, int num, bool skip)
     if (skip)
     {
         ref_difftest_regcpy(difftest_regs, 1);
-        printf("pc = %lx\n", difftest_regs[32]);
+        //printf("pc = %lx\n", difftest_regs[32]);
         return 0;
     }
 
